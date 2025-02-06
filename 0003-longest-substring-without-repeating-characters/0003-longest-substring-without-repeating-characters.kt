85466198663228da2a40e7ac17e64b6df27c675c
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-02-07 00:35:01 +0900

    Time: 9 ms (88.56%), Space: 38.4 MB (64.06%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.kt
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.kt
@@ -1,26 +1,18 @@
 class Solution {
     fun lengthOfLongestSubstring(s: String): Int {
-        var maxLength = 0
         var leftIndex = 0
-        var rightIndex = 0
+        var maxLength = 0
 
         val charMap = HashMap<Char, Int>()
 
-        while (rightIndex < s.length) {
-            val currentChar = s.get(rightIndex)
-            var currentCharCount = charMap.merge(currentChar, 1, Integer::sum)!!
-
-
-            while (currentCharCount > 1) {
-                var leftCharCount = charMap.getOrDefault(s.get(leftIndex), 0)
-                charMap.put(s.get(leftIndex), leftCharCount - 1)
-                leftIndex++
-                currentCharCount = charMap.getOrDefault(currentChar, 0)
+        s.forEachIndexed { index, charKey ->
+            if (charMap.contains(charKey)) {
+                leftIndex = Math.max(charMap.getOrDefault(charKey, 0) + 1, leftIndex)
             }
 
-            maxLength = Math.max(maxLength, rightIndex - leftIndex + 1)
-            rightIndex++
-        }
+            maxLength = Math.max(maxLength, index - leftIndex + 1)
+            charMap.put(charKey, index)
+        } 
 
         return maxLength
     }
