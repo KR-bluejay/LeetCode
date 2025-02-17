95c87be467acc715db13d24a1c0131c3de1f71b9
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-02-18 00:49:40 +0900

    Time: 190 ms (57.38%), Space: 38.7 MB (43.98%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.kt
@@ -0,0 +1,30 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun hasCycle(head: ListNode?): Boolean {
+        val itemSet = HashSet<Int>()
+        var slowNode: ListNode? = head;
+        var fastNode: ListNode? = head?.next;
+
+
+        while (slowNode != null && fastNode != null)  {
+            if (slowNode === fastNode) {
+                return true
+            }
+
+
+            slowNode = slowNode?.next
+            fastNode = fastNode?.next?.next
+        }
+
+        return false
+    }
+}
