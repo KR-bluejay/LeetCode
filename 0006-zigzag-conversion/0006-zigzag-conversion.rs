3c4d9208b9906cbfa311969b8edab29fe15c3877
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-03-19 20:51:15 +0900

    Time: 0 ms (100%), Space: 2.4 MB (79.81%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.rs
+++ 0006-zigzag-conversion/0006-zigzag-conversion.rs
@@ -1,39 +1,23 @@
 impl Solution {
     pub fn convert(s: String, num_rows: i32) -> String {
-        let mut row_vec: Vec<String> = Vec::with_capacity(num_rows as usize);
-        let mut s_iter = s.chars().peekable();
-
-        row_vec.resize(num_rows as usize, String::new());
-
-        loop {
-
-            if s_iter.peek().is_none() {
-                break;
-            }
-            
-            for i in 0 .. num_rows {
-                let opt_s_item = s_iter.next();
-
-                if let Some(s_item) = opt_s_item {
-                    row_vec[i as usize].push(s_item);
-                } else {
-                    break;
-                }
-            }
-
-            for i in (1 .. num_rows - 1).rev() {
-                let opt_s_item = s_iter.next();
-                
-
-                if let Some(s_item) = opt_s_item {
-                    row_vec[i as usize].push(s_item);
-                } else {
-                    break;
-                }
-            }
-
+        if num_rows == 1 {
+            return s;
         }
 
-        row_vec.join("")
+        let mut rows: Vec<String> = vec![String::new(); num_rows as usize];
+        
+        let mut cur_row = 0;
+        let mut direction = -1;
+
+        for s_item in s.chars() {
+            rows[cur_row as usize].push(s_item);
+
+            if cur_row == 0 || cur_row == num_rows - 1 {
+                direction *= -1;
+            }
+            cur_row += direction;
+        }
+
+        rows.concat()
     }
 }
