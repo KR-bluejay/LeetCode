8f9714ebe6d6e03ce07f2e07083985a5dda240e0
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-03-19 19:06:48 +0900

    Time: 0 ms (100%), Space: 2.3 MB (50.5%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.rs
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.rs
@@ -1,16 +1,5 @@
 impl Solution {
     pub fn reverse_words(mut s: String) -> String {
-        let mut rev_str = String::new();
-        let mut rev_iter = s.split_whitespace().rev().peekable();
-
-        while let Some(next_str) = rev_iter.next() {
-            rev_str += next_str;
-            
-            if rev_iter.peek().is_some() {
-                rev_str += " ";
-            }
-        }
-
-        rev_str
+        s.split_whitespace().rev().collect::<Vec<&str>>().join(" ")
     }
 }
