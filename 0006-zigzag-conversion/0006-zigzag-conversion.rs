05c50d529954e975f6a594223efe3998a539ec84
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-03-19 20:43:47 +0900

    Time: 112 ms (5.1%), Space: 2.5 MB (35.73%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.rs
@@ -0,0 +1,42 @@
+impl Solution {
+    pub fn convert(s: String, num_rows: i32) -> String {
+        let mut row_vec: Vec<String> = Vec::with_capacity(num_rows as usize);
+        let zigzag_num = 0;
+        let mut s_iter = s.chars().peekable();
+
+        row_vec.resize(num_rows as usize, String::new());
+
+        loop {
+
+            if s_iter.peek().is_none() {
+                break;
+            }
+            
+            for i in 0 .. num_rows {
+                let opt_s_item = s_iter.next();
+
+                if let Some(s_item) = opt_s_item {
+                    println!("st: {i} {s_item}");
+                    row_vec[i as usize] += &s_item.to_string();
+                } else {
+                    break;
+                }
+            }
+
+            for i in (1 .. num_rows - 1).rev() {
+                let opt_s_item = s_iter.next();
+                
+
+                if let Some(s_item) = opt_s_item {
+                    println!("Rev: {i} {s_item}");
+                    row_vec[i as usize] += &s_item.to_string();
+                } else {
+                    break;
+                }
+            }
+
+        }
+
+        row_vec.join("")
+    }
+}
