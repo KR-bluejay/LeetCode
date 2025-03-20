99ac7751420e727e1174a098e5a5d846af78d63e
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-03-20 21:27:35 +0900

    Time: 0 ms (100%), Space: 2.4 MB (36.5%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.rs
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.rs
@@ -1,58 +1,24 @@
-use std::cmp;
-
 impl Solution {
     pub fn longest_ones(nums: Vec<i32>, k: i32) -> i32 {
-        let mut left_index: i32 = 0;
-        let mut init_index: i32 = 0;
+        let mut left = 0;
+        let mut zero_count = 0;
+        let mut max_sum = 0;
 
-        let mut right_index: i32 = 0;
-        let mut flag_list: Vec<i32> = Vec::with_capacity(k as usize);
-
-
-        for i in 0 .. nums.len() {
-            if k == 0 {
-                break;
+        for right in 0 .. nums.len() {
+            if nums[right] == 0{
+                zero_count += 1;
             }
 
-            if nums[i] == 0 {
-                flag_list.push(i as i32);
-            }
-            init_index = i as i32;
-
-            if flag_list.len() == k as usize {
-                break;
-            }
-        }
-
-        
-
-        let mut max_sum: i32 = if init_index == 0 && k == 0 {
-            0
-        } else { 
-            (init_index - left_index + 1)
-        };
-
-
-
-        if init_index == 0 && max_sum == 0 {
-            init_index = -1;
-        }
-
-        for right_index in (init_index + 1) as usize .. nums.len() {
-            if nums[right_index] == 0 && k == 0 {
-                left_index = right_index as i32 + 1;
-                continue;
-            } else if nums[right_index] == 0 {
-                left_index = (flag_list[0] + 1);
-                flag_list[0] = right_index as i32;
-
-                flag_list.sort();
+            while zero_count > k {
+                if nums[left] == 0 {
+                    zero_count -= 1;
+                }
+                left += 1;
             }
 
-            let cur_sum = right_index as i32 - left_index + 1;
+            let cur_sum = right - left + 1;
 
-            println!("cur: {cur_sum}");
-            max_sum = cmp::max(cur_sum, max_sum);
+            max_sum = max_sum.max(cur_sum);
         }
 
         max_sum as i32
