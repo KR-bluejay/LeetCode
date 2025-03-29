d039e824760aeb1b14b81fcab01b565fe562e29b
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-03-29 16:43:32 +0900

    Time: 0 ms (100%), Space: 2.3 MB (46.67%) - LeetHub

--- 0078-subsets/0078-subsets.rs
+++ 0078-subsets/0078-subsets.rs
@@ -1,5 +1,5 @@
 impl Solution {
-    fn dfs(cur: &mut Vec<i32>, ans: &mut Vec<Vec<i32>>, visited: &mut Vec<bool>, nums: &[i32], len: usize) {
+    fn dfs(cur: &mut Vec<i32>, ans: &mut Vec<Vec<i32>>, nums: &[i32], len: usize) {
         if cur.len() == len {
             ans.push(cur.to_vec());
 
@@ -9,20 +9,18 @@ impl Solution {
         for i in 0 .. nums.len() {
             cur.push(nums[i]);
 
-            Self::dfs(cur, ans, visited, &nums[i+1..nums.len()], len);
+            Self::dfs(cur, ans, &nums[i+1..nums.len()], len);
 
             cur.pop();
         }
     }
     pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
         let mut ans: Vec<Vec<i32>> = Vec::new();
-        let mut visited: Vec<bool> = vec![false; nums.len()];
         
         ans.push(Vec::new());
 
         for i in 1 ..= nums.len() {
-            Self::dfs(&mut Vec::with_capacity(nums.len()), &mut ans, &mut visited, &nums, i) ;
-            std::mem::replace(&mut visited, vec![false; nums.len()]);
+            Self::dfs(&mut Vec::with_capacity(nums.len()), &mut ans, &nums, i) ;
         }
 
         ans
