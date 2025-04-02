2eeaa65666457721e084e39a622862a07cef69c2
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-02 15:14:16 +0900

    Time: 2 ms (5%), Space: 2.1 MB (82.5%) - LeetHub

---
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn maximum_triplet_value(nums: Vec<i32>) -> i64 {
+        let mut max_triple = 0;
+
+        for i in 0 .. nums.len() - 2 {
+            for j in i + 1 .. nums.len() - 1 {
+                for k in j + 1 .. nums.len() {
+                    max_triple = max_triple.max((nums[i] - nums[j]) as i64 * nums[k] as i64)
+                }
+            }
+        }
+        max_triple
+    }
+}
