b094ae85cb7545b23672de4d4a46ba2e8eadfa9c
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-03 16:32:43 +0900

    Time: 0 ms (100%), Space: 3.7 MB (73.33%) - LeetHub

--- 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.rs
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.rs
@@ -1,13 +1,15 @@
 impl Solution {
     pub fn maximum_triplet_value(nums: Vec<i32>) -> i64 {
-        let mut max_l = 0;
-        let mut max_m = 0;
-        let mut max_triplet = 0;
+        let mut max_l: i64 = 0;
+        let mut max_m: i64 = 0;
+        let mut max_triplet: i64 = 0;
 
         for i in 0 .. nums.len() {
-            max_triplet = max_triplet.max(max_m * nums[i] as i64);
-            max_m = max_m.max(max_l - nums[i] as i64);
-            max_l = max_l.max(nums[i] as i64);
+            let cur_num = nums[i] as i64;
+
+            max_triplet = max_triplet.max(max_m * cur_num);
+            max_m = max_m.max(max_l - cur_num);
+            max_l = max_l.max(cur_num);
         }
         max_triplet
     }
