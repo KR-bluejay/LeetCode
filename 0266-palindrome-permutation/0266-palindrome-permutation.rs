6476920477bdcfb8484370a1d51af49387947f61
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-04 17:42:42 +0900

    Time: 0 ms (100%), Space: 2.1 MB (60.53%) - LeetHub

--- 0266-palindrome-permutation/0266-palindrome-permutation.rs
+++ 0266-palindrome-permutation/0266-palindrome-permutation.rs
@@ -1,16 +1,12 @@
 use std::collections::{ HashMap };
+use itertools::Itertools;
 
 impl Solution {
     pub fn can_permute_palindrome(s: String) -> bool {
-        let mut char_map: HashMap<char, usize> = HashMap::new();
-
-        for s_item in s.chars() {
-            *char_map.entry(s_item).or_insert(0) += 1;
-        }
-
-        if s.len() % 2 == 0 {
-            return char_map.iter().all(|(k, v)| v % 2 == 0);
-        }
-        return char_map.iter().filter(|(k, v)| **v % 2 != 0).count() == 1;
+        let char_freq_map: HashMap<char, usize> = s.chars().fold(HashMap::new(), |mut map, c| {
+            *map.entry(c).or_insert(0) += 1;
+            map
+        });
+        char_freq_map.values().filter(|&v| v % 2 != 0).count() <= 1
     }
 }
