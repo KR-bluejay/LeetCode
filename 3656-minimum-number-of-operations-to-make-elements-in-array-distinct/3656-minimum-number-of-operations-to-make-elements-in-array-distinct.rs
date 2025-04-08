9091b6efe41b9c53b02ef604a015e7327e65067b
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-08 22:27:07 +0900

    Time: 0 ms (100%), Space: 2.1 MB (77.78%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.rs
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.rs
@@ -2,16 +2,15 @@ use std::collections::{ HashMap };
 
 impl Solution {
     pub fn minimum_operations(nums: Vec<i32>) -> i32 {
-        let mut op_count: i32 = 0;
+        let mut op_count: usize = 0;
         let mut num_map: HashMap<i32, usize> = HashMap::new();
 
         for (num_id, &num_item) in nums.iter().enumerate() {
             if let Some(old_num_id) = num_map.insert(num_item, num_id) {
-                println!("{num_item} {old_num_id} {num_id} {op_count}");
-                op_count = op_count.max(((old_num_id + 1) as f32 / 3 as f32).ceil() as i32)
+                op_count = op_count.max((old_num_id + 3) / 3);
             }
         }
 
-        op_count
+        op_count as i32
     }
 }
