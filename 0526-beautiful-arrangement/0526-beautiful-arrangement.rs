da2a0edd89a006419930ba2bd72366ceb4b620b6
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-09 02:41:16 +0900

    Time: 53 ms (16.67%), Space: 2 MB (100%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.rs
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.rs
@@ -3,46 +3,28 @@ impl Solution {
     fn dp(
         id: usize, 
         n: usize, 
-        location_map: &HashMap<usize, HashSet<usize>>, 
-        history: &mut Vec<usize>, 
-    ) -> usize {
-        let mut res = 0;
-
-
-        let avail_index_set = match location_map.get(&id) {
-            Some(v) => v,
-            None => {
-                println!("{id}");
-                println!("{history:?}");
-                return 1;
-            },
-        };
-
-        for &avail_index in avail_index_set.iter() {
-            if history.contains(&avail_index) {
-                continue;
-            }
-
-            history.push(avail_index);
-            res += Self::dp(id + 1, n, location_map, history);
-            history.pop();
+        visited: &mut Vec<bool>,
+        arrangement_count: &mut i32,
+    ) {
+        if id == n {
+            *arrangement_count += 1;
         }
 
-        return res;
+        for i in 1 ..= n {
+            if ((id + 1) % i == 0 || i % (id + 1) == 0) && !visited[i] {
+                visited[i] = true;
+                Self::dp(id + 1, n, visited, arrangement_count);
+                visited[i] = false;
+            }
+        }
     }
     pub fn count_arrangement(n: i32) -> i32 {
         let n = n as usize;
-        let mut location_map: HashMap<usize, HashSet<usize>> = HashMap::new();
-        let mut history: Vec<usize> = Vec::with_capacity(n);
+        let mut visited: Vec<bool> = vec![false; n + 1];
+        let mut arr_count = 0;
 
-        for i in 1 ..= n {
-            for j in 1 ..= n {
-                if i % j == 0 || j % i == 0 {
-                    location_map.entry(j - 1).or_insert(HashSet::new()).insert(i);
-                }
-            }
-        }
+        Self::dp(0, n, &mut visited, &mut arr_count);
 
-        Self::dp(0, n, &location_map, &mut history) as i32
+        arr_count
     }
 }
