b86fbdba0ad60d57abb09a432400de4d6af864db
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-09 15:52:05 +0900

    Time: 0 ms (100%), Space: 2.7 MB (27.27%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.rs
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.rs
@@ -1,11 +1,14 @@
 impl Solution {
     pub fn max_score_sightseeing_pair(values: Vec<i32>) -> i32 {
         let mut max_score = 0;
+        let mut max_left_score = 0;
 
-        for i in 0 .. values.len() - 1 {
-            for j in i + 1 .. values.len() {
-                max_score = max_score.max(values[i] + values[j] + i as i32 - j as i32)
-            }
+        for (score_id, &score_value) in values.iter().enumerate() {
+            let score_id_i32 = score_id as i32;
+            let cur_score = score_value - score_id_i32;
+
+            max_score = max_score.max(max_left_score + cur_score);
+            max_left_score = max_left_score.max(score_value + score_id_i32);
         }
         max_score
     }
