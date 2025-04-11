67e18d815b5eb1b842edccda3550f65b98f14f47
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-11 18:59:25 +0900

    Time: 4 ms (93.33%), Space: 2.2 MB (53.33%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.rs
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.rs
@@ -1,31 +1,20 @@
-use std::str::FromStr;
-
 impl Solution {
     pub fn count_symmetric_integers(low: i32, high: i32) -> i32 {
-        let mut num_count = 0;
+        let mut symmetric_count = 0;
 
-        for i in low ..= high {
-            let i_str: Vec<u32> = i.to_string().chars().map(|v| v.to_digit(10).unwrap()).collect();
-            let i_str_len = i_str.len();
-
-            if i_str_len % 2 != 0 {
+        for mid in low ..= high {
+            if mid < 100 && mid % 11 == 0 {
+                symmetric_count += 1;
                 continue;
-            }
-            let mut l_value = 0;
-            let mut r_value = 0;
-
-            for (i, &v) in i_str.iter().enumerate() {
-                if i < i_str_len / 2 {
-                    l_value+= v;
-                } else {
-                    r_value+= v;
+            } else if 1000 <= mid && mid < 10000 {
+                let left = (mid / 1000) + (mid % 1000) / 100;
+                let right = (mid % 100) / 10 + (mid % 10);
+    
+                if left == right {
+                    symmetric_count += 1;
                 }
             }
-
-            if l_value == r_value {
-                num_count += 1;
-            }
         }
-        num_count
+        symmetric_count
     }
 }
