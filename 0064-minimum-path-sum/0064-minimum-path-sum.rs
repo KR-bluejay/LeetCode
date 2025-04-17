f6b0649bc610f977039ba8c3b7e5c9d4712b5841
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-17 21:51:43 +0900

    Time: 0 ms (100%), Space: 2.6 MB (79.44%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.rs
+++ 0064-minimum-path-sum/0064-minimum-path-sum.rs
@@ -9,26 +9,16 @@ impl Solution {
             let size = grid.len() - 1;
             grid[size][i] += grid[size][i + 1];
         }
-        println!("{grid:?}");
+        
         for i in (0 .. grid.len() - 1).rev() {
             for j in (0 .. grid[0].len() - 1).rev() {
                 if i == j {
                     grid[i][j] += grid[i + 1][j].min(grid[i][j + 1]);
-                    continue;
-                }
-
-                println!("{i} {j} {}", grid[i][j]);
-
-                if i + 1 < grid.len() && j + 1 < grid[i].len() {
+                } else if i + 1 < grid.len() && j + 1 < grid[i].len() {
                     grid[i][j] += grid[i + 1][j].min(grid[i][j + 1]);
                 }
-
-                // if i + 1 < grid[j].len() && j + 1 < grid.len() {
-                //     grid[j][i] += grid[j][i + 1].min(grid[j + 1][i]);
-                // }
             }
         }
-        println!("{grid:?}");
         grid[0][0]
     }
 }
