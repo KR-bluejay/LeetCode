10f421b8604ec92ad74ac30c8c0e9718bcf55546
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-17 16:55:20 +0900

    Time: 39 ms (15.04%), Space: 10.6 MB (5.26%) - LeetHub

---
+++ 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.rs
@@ -0,0 +1,58 @@
+use std::collections::HashMap;
+
+
+impl Solution {
+    fn query(bit: &Vec<i64>, num_id: usize) -> i64 {
+        let mut num_id = num_id as i32;
+        let mut count: i64 = 0;
+
+        while num_id > 0 {
+            count += bit[num_id as usize];
+            num_id -= num_id & (num_id * - 1);
+        }
+
+        count
+    }
+    fn update(bit: &mut Vec<i64>, num_id: usize) {
+        let mut num_id = num_id as i32;
+        let size = bit.len() as i32;
+
+        while num_id < size {
+            bit[num_id as usize] += 1;
+            num_id += num_id & (num_id * -1);
+        }
+    }
+    pub fn good_triplets(nums1: Vec<i32>, nums2: Vec<i32>) -> i64 {
+        let mut bit1: Vec<i64> = vec![0; nums1.len() + 1];
+        let mut bit2: Vec<i64> = vec![0; nums1.len() + 1];
+        let mut num2_mapping: HashMap<i32, usize> = HashMap::new();
+        let mut res = 0;
+
+        let mut smaller: Vec<i64> = vec![0; nums1.len()];
+        let mut larger: Vec<i64> = vec![0; nums1.len()];
+
+        for (num2_id, num_item) in nums2.iter().enumerate() {
+            num2_mapping.insert(*num_item, num2_id);
+        }
+
+        for i in 0 .. nums1.len() {
+            let num2_id = num2_mapping.get(&nums1[i]).unwrap();
+            let num2_id_rev = num2_mapping.get(&nums1[nums1.len() - 1 - i]).unwrap();
+            
+            smaller[*num2_id] = Self::query(&bit1, *num2_id + 1);
+            Self::update(&mut bit1, *num2_id + 1);
+
+            let seen = Self::query(&bit2, nums1.len());
+            larger[*num2_id_rev] = seen - Self::query(&bit2, *num2_id_rev + 1);
+
+            Self::update(&mut bit2, *num2_id_rev + 1);
+        }
+
+
+        for i in 0 .. larger.len() {
+            res += larger[i] * smaller[i];
+        }
+
+        res
+    }
+}
