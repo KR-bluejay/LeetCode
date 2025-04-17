35885cf860b3a1943e5f165c0cddf7c909f2795f
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-17 20:41:13 +0900

    Time: 0 ms (100%), Space: 2.4 MB (82.28%) - LeetHub

--- 0120-triangle/0120-triangle.rs
+++ 0120-triangle/0120-triangle.rs
@@ -1,26 +1,11 @@
 impl Solution {
-    fn explore_path(
-        row_id: usize, 
-        column_id: usize, 
-        triangle: &Vec<Vec<i32>>, 
-        cache: &mut Vec<Vec<i32>>
-    ) -> i32 {
-        if row_id >= triangle.len() || column_id >= triangle[row_id].len() {
-            return 0;
+    pub fn minimum_total(mut triangle: Vec<Vec<i32>>) -> i32 {
+        for i in (0 .. triangle.len() - 1).rev() {
+            for j in (0 .. triangle[i].len()) {
+                triangle[i][j] += triangle[i + 1][j].min(triangle[i + 1][j + 1]);
+            }
         }
 
-
-        if cache[row_id][column_id] != i32::MIN {
-            return cache[row_id][column_id];
-        }
-
-        cache[row_id][column_id] = triangle[row_id][column_id] + Self::explore_path(row_id + 1, column_id, triangle, cache).min(Self::explore_path(row_id + 1, column_id + 1, triangle, cache));
-
-
-        cache[row_id][column_id]
-    }
-    pub fn minimum_total(triangle: Vec<Vec<i32>>) -> i32 {
-        let mut cache: Vec<Vec<i32>> = vec![vec![i32::MIN; triangle[triangle.len() - 1].len()]; triangle.len()];
-        Self::explore_path(0, 0, &triangle, &mut cache)
+        triangle[0][0]
     }
 }
