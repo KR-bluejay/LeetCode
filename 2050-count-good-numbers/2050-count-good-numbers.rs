28399405782a32d87e42a78ee083b4c9ce8b9908
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-18 16:15:25 +0900

    Time: 0 ms (100%), Space: 2.3 MB (6.9%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.rs
+++ 2050-count-good-numbers/2050-count-good-numbers.rs
@@ -1,21 +1,18 @@
 impl Solution {
-    
-    fn count_number(mut a: i64, mut b: i64) -> i64 {
-        let mut ret: i64 = 1;
+    fn get_mod(mut num: i64, mut pow_count: i64) -> i64 {
+        let mut res: i64 = 1;
 
-        while b > 0  {
-            if b % 2 == 1 {
-                ret = (ret * a) % (10_i64.pow(9) + 7);
+        while pow_count > 0 {
+            if pow_count % 2 == 1 {
+                res = res * num % 1000000007;
+                pow_count -= 1;
             }
-
-            a = a * a % (10_i64.pow(9) + 7);
-            b /= 2;
+            num = num * num % 1000000007;
+            pow_count /= 2;
         }
-
-        ret
+        res % 1000000007
     }
     pub fn count_good_numbers(n: i64) -> i32 {
-        (Self::count_number(5, (n + 1) / 2) * Self::count_number(4, n / 2) % (10_i64.pow(9) + 7)) as i32
+        (Self::get_mod(5, (n + 1) / 2) * Self::get_mod(4, n / 2)  % 1000000007) as i32
     }
-    
 }
