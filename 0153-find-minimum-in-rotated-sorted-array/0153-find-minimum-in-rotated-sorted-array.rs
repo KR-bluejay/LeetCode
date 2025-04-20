a7557b1c239786d074b6c24391a869277ea197ec
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-20 20:06:57 +0900

    Time: 0 ms (100%), Space: 2.1 MB (88.79%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.rs
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.rs
@@ -4,36 +4,13 @@ impl Solution {
         let mut right = nums.len() - 1;
 
 
-        while left <= right {
+        while left < right {
             let mid = left + (right - left) / 2;
 
-            let is_left_sorted = nums[left] <= nums[mid];
-            let is_right_sorted = nums[mid] <= nums[right];
-
-
-            if is_left_sorted && is_right_sorted {
-                return nums[left];
-            }
-
-            if is_right_sorted {
-                if mid == 0 {
-                    return 0;
-                }
-
-                if nums[mid] < nums[mid - 1] {
-                    return nums[mid];
-                }
-                right = mid - 1;
-            } else {
-                if mid + 1 == nums.len() {
-                    return nums[mid];
-                }
-
-                if nums[mid] > nums[mid + 1] {
-                    return nums[mid + 1];
-                }
-
+            if nums[mid] > nums[right] {
                 left = mid + 1;
+            } else {
+                right = mid;
             }
         }
 
