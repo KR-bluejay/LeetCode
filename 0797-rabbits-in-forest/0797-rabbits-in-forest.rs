80a3addf1d71c2a7939a0e30f25e4aee4efe848b
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-20 15:50:42 +0900

    Time: 0 ms (100%), Space: 2.3 MB (100%) - LeetHub

---
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn num_rabbits(answers: Vec<i32>) -> i32 {
+        let mut color_map: HashMap<i32, i32> = HashMap::new();
+        let mut rabbit_count: i32 = 0;
+
+        for &answer_item in answers.iter() {
+            if answer_item == 0 {
+                rabbit_count += 1;
+
+                continue;
+            }
+            let mut res = *color_map.entry(answer_item).and_modify(|v| *v += 1).or_insert(0);
+
+            if res == answer_item {
+                rabbit_count += (answer_item + 1);
+                color_map.remove(&answer_item);
+            }
+        }
+
+        for (color_key, color_count) in color_map.iter() {
+            rabbit_count += color_key + 1;
+        }
+
+        rabbit_count
+    }
+}
