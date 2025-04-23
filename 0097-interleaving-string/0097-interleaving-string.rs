82a41855ad32cfc01bfcce47398caca83d0d6022
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-23 19:55:29 +0900

    Time: 1 ms (77.59%), Space: 2.3 MB (44.83%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.rs
+++ 0097-interleaving-string/0097-interleaving-string.rs
@@ -9,32 +9,41 @@ impl Solution {
         dp: &mut Vec<Vec<bool>>
     ) -> bool {
         if s3_id == s3.len() {
-            if s1_id == s1.len() && s2_id == s2.len() {
-                return true;
-            } else {
-                return false;
-            }
+            return s1_id == s1.len() && s2_id == s2.len();
         }
-        let mut flag = false;
 
         if dp[s1_id][s2_id] == false {
             return false;
         }
 
-        if s1_id < s1.len() && s1[s1_id] == s3[s3_id] {
-            if Self::try_interleave(s1, s2, s3, s1_id + 1, s2_id, s3_id + 1, dp) {
-                return true;
-            }
+        if s1_id < s1.len() && s1[s1_id] == s3[s3_id] 
+        && Self::try_interleave(
+            s1, 
+            s2, 
+            s3, 
+            s1_id + 1, 
+            s2_id, 
+            s3_id + 1, 
+            dp
+        ) {
+            return true;
         }
 
-        if s2_id < s2.len() && s2[s2_id] == s3[s3_id] {
-            if  Self::try_interleave(s1, s2, s3, s1_id, s2_id + 1, s3_id + 1, dp) {
-                return true;
-            }
+        if s2_id < s2.len() && s2[s2_id] == s3[s3_id] 
+        && Self::try_interleave(
+                s1, 
+                s2, 
+                s3, 
+                s1_id, 
+                s2_id + 1, 
+                s3_id + 1, 
+                dp
+            ) {
+            return true;
         }
 
         dp[s1_id][s2_id] = false;
-        return false;
+        false
     }
     pub fn is_interleave(s1: String, s2: String, s3: String) -> bool {
         let s1: Vec<char> = s1.chars().collect();
