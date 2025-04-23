f2ec4bbc8e5a4d5087400c2396b2475223b4c910
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-23 18:51:50 +0900

    Time: 0 ms (100%), Space: 2.7 MB (93.33%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>, fee: i32) -> i32 {
+        let mut tx_hold: Vec<i32> = vec![0; prices.len()];
+        let mut tx_sell: Vec<i32> = vec![0; prices.len()];
+
+        tx_hold[0] = prices[0] * -1;
+        tx_sell[0] = 0;
+
+        for i in 1 .. prices.len() {
+            tx_hold[i] = tx_hold[i - 1].max(tx_sell[i - 1] - prices[i]);
+            tx_sell[i] = tx_sell[i - 1].max(tx_hold[i - 1] + prices[i] - fee);
+        }
+
+        *tx_hold.last().unwrap().max(tx_sell.last().unwrap())
+    }
+}
