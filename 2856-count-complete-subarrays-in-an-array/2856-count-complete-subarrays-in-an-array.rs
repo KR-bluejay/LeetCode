f3945e35dfdabd490c72f96058b502f7d06862b5
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-24 15:37:52 +0900

    Time: 0 ms (100%), Space: 2.2 MB (100%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.rs
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.rs
@@ -1,44 +1,38 @@
-use std::collections::{ HashSet, BTreeSet };
+use std::collections::{HashSet, HashMap};
+
 impl Solution {
-    fn find_subarray(
-        start_id: usize, 
-        cur_id: usize,
-        nums: &Vec<i32>, 
-        subset: &mut BTreeSet<i32>,
-        num_dist_count: usize,
-        visited: &mut Vec<Vec<bool>>
-    ) -> usize {
-        if cur_id == nums.len() || visited[start_id][cur_id] {
-            return 0;
-        }
-
-        let num_item = nums[cur_id];
-
-        visited[start_id][cur_id] = true;
-        
-        subset.insert(nums[cur_id]);
-        let contain_count: usize = if subset.len() == num_dist_count {
-            visited[start_id][cur_id .. nums.len()].fill(true);
-            nums.len() - cur_id
-        } else {
-            Self::find_subarray(start_id, cur_id + 1, nums, subset, num_dist_count, visited)
-        };
-
-        
-        subset.clear();
-        // Skip
-        let skip_count = Self::find_subarray(cur_id + 1, cur_id + 1, nums, subset, num_dist_count, visited);
-        
-
-        skip_count + contain_count
-    }
     pub fn count_complete_subarrays(nums: Vec<i32>) -> i32 {
         let num_set: HashSet<_> = nums.iter().collect();
         let num_dist_count = num_set.len();
-        let mut visited: Vec<Vec<bool>> = vec![vec![false; nums.len()]; nums.len()];
-        let mut subset: BTreeSet<i32> = BTreeSet::new();
+        
+        let mut right_id: usize = 0;
+        let mut complete_count: usize = 0;
+        let mut num_count_map: HashMap<i32, i32> = HashMap::with_capacity(num_dist_count);
 
+        for left_id in 0 .. (nums.len() - num_dist_count + 1) {
+            if 0 < left_id {
+                let prev_num_key = nums[left_id - 1];
+                let mut num_value = num_count_map.get_mut(&prev_num_key).unwrap();
 
-        Self::find_subarray(0, 0, &nums, &mut subset, num_dist_count, &mut visited) as i32
+                if *num_value <= 1 {
+                    num_count_map.remove(&prev_num_key);
+                } else {
+                    *num_value -= 1;
+                }
+            }
+
+            while right_id < nums.len() 
+            && num_count_map.len() < num_dist_count {
+                num_count_map.entry(nums[right_id])
+                    .and_modify(|v| *v += 1)
+                    .or_insert(1);
+                right_id += 1;
+            }
+            if num_count_map.len() == num_dist_count {
+                complete_count += nums.len() - right_id + 1;
+            }
+        }
+
+        complete_count as i32
     }
 }
