738bb4fd5e3d74b4acd40bff7db726afff3c6114
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-24 14:53:18 +0900

    Time: 490 ms (11.11%), Space: 3.6 MB (0%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.rs
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.rs
@@ -14,14 +14,8 @@ impl Solution {
 
         let num_item = nums[cur_id];
 
-        visited[cur_id][cur_id] = true;
         visited[start_id][cur_id] = true;
-
-        // Skip
-        let skip_count = Self::find_subarray(cur_id, cur_id + 1, nums, &mut BTreeSet::from([num_item]), num_dist_count, visited);
         
-        
-
         subset.insert(nums[cur_id]);
         let contain_count: usize = if subset.len() == num_dist_count {
             visited[start_id][cur_id .. nums.len()].fill(true);
@@ -30,6 +24,11 @@ impl Solution {
             Self::find_subarray(start_id, cur_id + 1, nums, subset, num_dist_count, visited)
         };
 
+        
+        subset.clear();
+        // Skip
+        let skip_count = Self::find_subarray(cur_id + 1, cur_id + 1, nums, subset, num_dist_count, visited);
+        
 
         skip_count + contain_count
     }
