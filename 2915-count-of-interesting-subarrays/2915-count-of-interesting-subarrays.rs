c7816caf949870634fb5921f8f35388575b5deaa
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-25 22:03:02 +0900

    Time: 23 ms (50%), Space: 4.3 MB (100%) - LeetHub

---
+++ 2915-count-of-interesting-subarrays/2915-count-of-interesting-subarrays.rs
@@ -0,0 +1,34 @@
+use std::collections::{BTreeMap};
+impl Solution {
+    pub fn count_interesting_subarrays(
+        nums: Vec<i32>, 
+        modulo: i32, 
+        k: i32
+    ) -> i64 {
+        let mut prefix_sum = 0;
+        let mut count_map: BTreeMap<i32, i32> = BTreeMap::new();
+        let mut total_sum = 0;
+
+        count_map.insert(0, 1);
+
+        for (num_id, &num_val) in nums.iter().enumerate() {
+            prefix_sum += if num_val % modulo == k {
+                1
+            } else {
+                0
+            };
+            prefix_sum %= modulo;
+            
+            let target = (prefix_sum - k + modulo) % modulo;
+
+
+            if let Some(v) = count_map.get(&target) {
+                total_sum += *v as i64
+            }
+            count_map.entry(prefix_sum)
+                .and_modify(|v| *v += 1)
+                .or_insert(1);
+        }
+        total_sum
+    }
+}
