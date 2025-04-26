6cb77dd189196ab65dfc1ebe4fe15431ed9b06bd
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-26 14:23:39 +0900

    Time: 0 ms (100%), Space: 3.3 MB (90%) - LeetHub

--- 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.rs
+++ 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.rs
@@ -2,30 +2,33 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn count_subarrays(nums: Vec<i32>, min_k: i32, max_k: i32) -> i64 {
-        let mut k_map: HashMap<i32, usize> = HashMap::new();
         let mut valid_id = 0;
         let mut bound_count: i64 = 0;
+        let mut min_id: i64 = -1;
+        let mut max_id: i64 = -1;
 
         for (num_id, &num_item) in nums.iter().enumerate() {
             if num_item < min_k || max_k < num_item {
-                valid_id = num_id + 1;
-                k_map.clear();
+                min_id = -1;
+                max_id = -1;
+
+                valid_id = (num_id + 1) as i64;
+                
                 continue;
             }
 
             if num_item == min_k {
-                k_map.insert(min_k, num_id);
-            } else if num_item == max_k {
-                k_map.insert(max_k, num_id);
+                min_id = num_id as i64;
+            }
+            if num_item == max_k {
+                max_id = num_id as i64;
             }
 
-            if !k_map.contains_key(&min_k) || !k_map.contains_key(&max_k) {
+            if min_id == -1 || max_id == -1 {
                 continue;
             }
 
-            let min_k_id = k_map.get(&min_k).unwrap();
-            let max_k_id = k_map.get(&max_k).unwrap();
-            let target_id = min_k_id.min(max_k_id);
+            let target_id = min_id.min(max_id);
 
             bound_count += (target_id - valid_id + 1) as i64; 
         }
