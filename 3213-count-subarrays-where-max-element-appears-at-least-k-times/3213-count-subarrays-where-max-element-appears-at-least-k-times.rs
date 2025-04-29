3724f99f5c5c368153514a07c7e659a512bc90ac
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-04-29 15:33:38 +0900

    Time: 0 ms (100%), Space: 3.8 MB (12.5%) - LeetHub

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.rs
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.rs
@@ -1,74 +1,24 @@
-use std::collections::{ BinaryHeap };
-use std::cmp::{Ordering};
-
-#[derive(Debug, Eq, PartialEq)]
-struct NumItem {
-    id: usize,
-    val: i32,
-}
-
-impl Ord for NumItem {
-    fn cmp(&self, other: &Self) -> Ordering {
-        if self.val == other.val {
-            other.id.cmp(&self.id)
-        } else {
-            self.val.cmp(&other.val)
-        }
-    }
-}
-
-impl PartialOrd for NumItem {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
+use std::collections::BTreeMap;
 
 impl Solution {
     pub fn count_subarrays(nums: Vec<i32>, k: i32) -> i64 {
-        // Vector
         let k = k as usize;
-        let mut p_queue = BinaryHeap::new();
-        let mut max_count = 0;
-
+        let num_max = *nums.iter().max().unwrap_or(&i32::MAX);
+        let mut num_max_id_list: Vec<usize> = Vec::with_capacity(nums.len());
+        let mut num_count = 0;
 
         for (num_id, &num_val) in nums.iter().enumerate() {
-            let max_num_item = p_queue.peek().unwrap_or(
-                &NumItem {
-                    id: 0,
-                    val: 0
-                }
-            );
-
-            if max_num_item.val < num_val {
-                max_count = 0;
-                p_queue.clear();
-                p_queue.push(NumItem {
-                    id: num_id,
-                    val: num_val
-                });
-            } else if max_num_item.val == num_val {
-                p_queue.push(NumItem {
-                    id: num_id,
-                    val: num_val
-                });
+            if num_val == num_max {
+                num_max_id_list.push(num_id);
             }
 
-            if p_queue.len() > k {
-                p_queue.pop();
+            if num_max_id_list.len() < k {
+                continue;
             }
-            
-            let max_num_item = p_queue.peek().unwrap_or(
-                &NumItem {
-                    id: 0,
-                    val: 0
-                }
-            );
-
-
-            if p_queue.len() == k {
-                max_count += max_num_item.id + 1;
-            }
+            num_count += num_max_id_list[num_max_id_list.len() - k] + 1;
         }
-        max_count as i64
+        num_count as i64
+
+
     }
 }
