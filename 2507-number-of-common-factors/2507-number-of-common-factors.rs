22b388826aadb81a2d2a94751546a54178a84ab5
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-05-04 14:30:08 +0900

    Time: 0 ms (100%), Space: 2.2 MB (71.43%) - LeetHub

--- 2507-number-of-common-factors/2507-number-of-common-factors.rs
+++ 2507-number-of-common-factors/2507-number-of-common-factors.rs
@@ -8,41 +8,15 @@ impl Solution {
         a as usize
     }
     pub fn common_factors(a: i32, b: i32) -> i32 {
-        let mut factor_count = 1;
+        let mut factor_count: i32 = 0;
         let mut gcd = Self::gcd(a, b);
 
-        let mut sieve: Vec<usize> = vec![1; gcd + 1];
-
-        for i in 2 ..= gcd {
-            if sieve[i] != 1 {
-                continue;
-            }
-            for j in 1 ..= (gcd / i) {
-                if sieve[i * j] != 1 {
-                    continue;
-                }
-                sieve[i * j] = i;
+        for i in 1 ..= gcd {
+            if gcd % i == 0 {
+                factor_count += 1;
             }
         }
 
-        let prime_list: Vec<usize> = sieve.iter().enumerate().filter(|(i, v)| **v > 1 && *i == **v).map(|(_, v)| *v).collect();
-
-
-        for &prime_item in prime_list.iter() {
-            if gcd <= 1 || prime_item > gcd {
-                break;
-            }
-            let mut count = 0;
-
-            while gcd > 1 && prime_item <= gcd && gcd % prime_item == 0 {
-                count += 1;
-                gcd /= prime_item;
-            }
-            count += 1;
-            
-
-            factor_count *= count;
-        }
-        factor_count as i32
+        factor_count
     }
 }
