3eb666f258816bdfbdc76f8e2df0cd8d978a3178
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-08-29 09:21:55 +0900

    Time: 0 ms (100%), Space: 2.4 MB (50%) - LeetHub

---
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.rs
@@ -0,0 +1,66 @@
+impl Solution {
+    pub fn sort_matrix(mut grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let grid_len = grid.len();
+
+        for init_id in (0 .. grid_len) {
+            let mut row_id = init_id;
+            let mut col_id = 0;
+            let mut temp = Vec::with_capacity(grid_len - init_id);
+
+            while (row_id < grid_len && col_id < grid_len) {
+                temp.push(grid[row_id][col_id]);
+                row_id += 1;
+                col_id += 1;
+            }
+            temp.sort_by(|a, b| b.cmp(a));
+
+            
+            let mut row_id = init_id;
+            let mut col_id = 0;
+            let mut temp_id = 0;
+
+            loop {
+                if temp_id < temp.len() {
+                    grid[row_id][col_id] = temp[temp_id];
+                    temp_id += 1;
+                    row_id += 1;
+                    col_id += 1;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        for init_id in (1 .. grid_len) {
+            let mut row_id = 0;
+            let mut col_id = init_id;
+            let mut temp = Vec::with_capacity(grid_len - init_id);
+
+            while (row_id < grid_len && col_id < grid_len) {
+                temp.push(grid[row_id][col_id]);
+
+                row_id += 1;
+                col_id += 1;
+            }
+            temp.sort();
+
+
+            
+            let mut row_id = 0;
+            let mut col_id = init_id;
+            let mut temp_id = 0;
+
+            loop {
+                if temp_id < temp.len() {
+                    grid[row_id][col_id] = temp[temp_id];
+                    temp_id += 1;
+                    row_id += 1;
+                    col_id += 1;
+                } else {
+                    break;
+                }
+            }
+        }
+        grid
+    }
+}
