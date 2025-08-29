72e442e39a92cfba9379e927f7c5eee145e45fcf
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-08-29 09:31:27 +0900

    Time: 0 ms (100%), Space: 2.4 MB (50%) - LeetHub

--- 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.rs
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.rs
@@ -2,51 +2,47 @@ impl Solution {
     pub fn sort_matrix(mut grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         let grid_len = grid.len();
 
-        for init_id in (0 .. grid_len) {
-            let mut row_id = init_id;
-            let mut col_id = 0;
-            let mut temp = Vec::with_capacity(grid_len - init_id);
+        for start_row in (0 .. grid_len) {
+            let (mut row_id, mut col_id) = (start_row, 0);
+            let mut diagonal = Vec::with_capacity(grid_len - start_row);
 
             while (row_id < grid_len && col_id < grid_len) {
-                temp.push(grid[row_id][col_id]);
+                diagonal.push(grid[row_id][col_id]);
                 row_id += 1;
                 col_id += 1;
             }
-            temp.sort_by(|a, b| b.cmp(a));
+            diagonal.sort_by(|a, b| b.cmp(a));
 
             
-            let mut row_id = init_id;
-            let mut col_id = 0;
-            let mut temp_id = 0;
-            
-            for temp_id in 0 .. temp.len() {
-                grid[row_id][col_id] = temp[temp_id];
+            let (mut row_id, mut col_id) = (start_row, 0);
+
+            for diagonal_item in diagonal.iter() {
+                grid[row_id][col_id] = *diagonal_item;
+                
                 row_id += 1;
                 col_id += 1;
             }
-
         }
 
-        for init_id in (1 .. grid_len) {
-            let mut row_id = 0;
-            let mut col_id = init_id;
-            let mut temp = Vec::with_capacity(grid_len - init_id);
+        for start_col in (1 .. grid_len) {
+            let (mut row_id, mut col_id) = (0, start_col);
+            let mut diagonal = Vec::with_capacity(grid_len - start_col);
 
             while (row_id < grid_len && col_id < grid_len) {
-                temp.push(grid[row_id][col_id]);
+                diagonal.push(grid[row_id][col_id]);
 
                 row_id += 1;
                 col_id += 1;
             }
-            temp.sort();
+            diagonal.sort();
 
 
             
-            let mut row_id = 0;
-            let mut col_id = init_id;
-
-            for temp_id in 0 .. temp.len() {
-                grid[row_id][col_id] = temp[temp_id];
+            let (mut row_id, mut col_id) = (0, start_col);
+            
+            for diagonal_item in diagonal.iter() {
+                grid[row_id][col_id] = *diagonal_item;
+                
                 row_id += 1;
                 col_id += 1;
             }
