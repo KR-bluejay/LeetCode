f2819cbf2ac650fae3aa854ba6193f1576137082
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-09-04 16:42:34 +0900

    Time: 155 ms (18.18%), Space: 2.2 MB (81.82%) - LeetHub

--- 1067-campus-bikes-ii/1067-campus-bikes-ii.rs
+++ 1067-campus-bikes-ii/1067-campus-bikes-ii.rs
@@ -1,38 +1,46 @@
 impl Solution {
-    fn dyn_man_dist(
-        workers: &[Vec<i32>], 
-        bikes: &Vec<Vec<i32>>, 
-        bikes_use: &mut Vec<bool>,
-        mut min_dist: i32, 
-        mut cur_dist: i32
-    ) -> i32 {
-        if cur_dist > min_dist {
-            return i32::MAX;
+    fn dyn_assign(
+        workers: &[Vec<i32>],
+        bikes: &Vec<Vec<i32>>,
+        bit_mask: usize, 
+        mut ongoing_dist: i32,
+        short_dist: &mut i32
+    ) {
+        if ongoing_dist >= *short_dist {
+            return;
         }
 
         if workers.len() == 0 {
-            return cur_dist;
+            *short_dist = ongoing_dist;
+
+            return;
         }
 
-
-        for i in 0 .. bikes.len() {
-            if bikes_use[i] {
+        for bike_id in 0 .. bikes.len() {
+            let is_used_bike = (bit_mask & (1 << bike_id)) != 0;
+            
+            if is_used_bike {
                 continue;
             }
-            bikes_use[i] = true;
-            let work_bike_dist = (workers[0][0] - bikes[i][0]).abs() + (workers[0][1] - bikes[i][1]).abs();
-            let new_dist = Self::dyn_man_dist(&workers[1 ..], bikes, bikes_use, min_dist, cur_dist + work_bike_dist);
-            min_dist = min_dist.min(new_dist);
-            bikes_use[i] = false;
+
+            let cur_dist = ongoing_dist + (workers[0][0] - bikes[bike_id][0]).abs() 
+            + (workers[0][1] - bikes[bike_id][1]).abs();
+
+
+            Self::dyn_assign(
+                &workers[1 ..], 
+                bikes, 
+                bit_mask ^ (1 << bike_id), 
+                cur_dist, 
+                short_dist
+            );
         }
-
-
-        min_dist
     }
     pub fn assign_bikes(workers: Vec<Vec<i32>>, bikes: Vec<Vec<i32>>) -> i32 {
-        let mut min_dist: i32 = i32::MAX;
-        let mut bikes_use: Vec<bool> = vec![false; bikes.len()];
+        let mut short_dist = i32::MAX;
 
-        Self::dyn_man_dist(&workers[0 ..], &bikes, &mut bikes_use, i32::MAX, 0)
+        Self::dyn_assign(&workers[0 ..], &bikes, 0, 0, &mut short_dist);
+
+        short_dist
     }
 }
