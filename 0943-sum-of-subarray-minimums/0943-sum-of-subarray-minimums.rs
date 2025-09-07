ad45b44a562354fa50333bc7dc7c06fa13eec145
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-09-07 22:46:31 +0900

    Time: 21 ms (7.69%), Space: 4.2 MB (7.69%) - LeetHub

---
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.rs
@@ -0,0 +1,68 @@
+use std::ops::Range;
+use std::collections::{ HashMap };
+
+impl Solution {
+
+    pub fn sum_subarray_mins(arr: Vec<i32>) -> i32 {
+        let mut min_sum: i64 = 0;
+        let mut sub_stack: Vec<usize> = Vec::with_capacity(arr.len());
+        let mut sub_map: HashMap<usize, Range<usize>> = arr.iter().enumerate().map(|(i, v)| {
+            (i, Range {
+                start: i,
+                end: i,
+            })
+        }).collect();
+
+        for (num_id, num_val) in arr.iter().enumerate() {
+            while let Some(stack_id) = sub_stack.pop() {
+                let stack_num = arr[stack_id];
+                if stack_num < *num_val {
+                    sub_stack.push(stack_id);
+                    
+                    break;
+                }
+
+                sub_map.entry(stack_id).and_modify(|v| v.end = num_id - 1);
+            }
+            sub_stack.push(num_id);
+        }
+
+        while let Some(stack_num) = sub_stack.pop() {
+            sub_map.entry(stack_num).and_modify(|v| v.end = arr.len() - 1);
+        }
+
+        
+        for (num_id, num_val) in arr.iter().enumerate().rev() {
+            while let Some(stack_id) = sub_stack.pop() {
+                let stack_num = arr[stack_id];
+
+                if stack_num <= *num_val {
+                    sub_stack.push(stack_id);
+                    
+                    break;
+                }
+
+                sub_map.entry(stack_id).and_modify(|v| v.start = num_id + 1);
+            }
+            sub_stack.push(num_id);
+        }
+
+        while let Some(stack_num) = sub_stack.pop() {
+            sub_map.entry(stack_num).and_modify(|v| v.start = 0);
+        }
+
+        for (sub_id, sub_range) in sub_map.iter() {
+            let Range {start, end} = sub_range;
+            let sub_val = arr[*sub_id];
+
+            let range_count = (end - sub_id + 1) * (sub_id - start + 1);
+
+            min_sum += (sub_val as i64 * range_count as i64) % 1000000007;
+            min_sum %= 1000000007;
+        }
+
+
+
+        min_sum as i32
+    }
+}
