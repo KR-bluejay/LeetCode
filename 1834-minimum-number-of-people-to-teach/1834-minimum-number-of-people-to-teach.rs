2669c8e0a2705bd32eb9c31d5217e982e4942a8e
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-09-10 18:42:23 +0900

    Time: 24 ms (100%), Space: 3.7 MB (0%) - LeetHub

--- 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.rs
+++ 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.rs
@@ -1,81 +1,40 @@
-use std::cmp::Ordering;
-use std::collections::{ BTreeSet, HashSet};
-
+use std::collections::HashSet;
 
 impl Solution {
     pub fn minimum_teachings(
-        lang_count: i32, 
+        n: i32, 
         mut languages: Vec<Vec<i32>>, 
-        mut friendships: Vec<Vec<i32>>
+        friendships: Vec<Vec<i32>>
     ) -> i32 {
-        let mut friendship_remove_set: BTreeSet<usize> = BTreeSet::new();
-        let mut teaching_friend_set: HashSet<usize> = HashSet::new();
-        let mut teaching_lang_set: HashSet<i32> = HashSet::new();
+        let mut affected_user_set: HashSet<usize> = HashSet::with_capacity(languages.len());
+        let user_langs: Vec<HashSet<i32>> = languages.into_iter()
+            .map(|v| v.into_iter().collect())
+            .collect();
 
-        for lang_id in 0 .. languages.len() {
-            languages[lang_id].sort();
-        }
+        for friendship in &friendships {
+            let first_id = friendship[0] as usize - 1;
+            let second_id = friendship[1] as usize - 1;
 
-        for (friendship_id, friendship) in friendships.iter().enumerate() {
-            let left_id = friendship[0] as usize - 1;
-            let right_id = friendship[1] as usize - 1;
-
-            let left_langs = &languages[left_id];
-            let right_langs = &languages[right_id];
-
-
-            let mut left_lang_id: usize = 0;
-            let mut right_lang_id: usize = 0;
-
-            while left_lang_id < left_langs.len() && right_lang_id < right_langs.len() {
-                match left_langs[left_lang_id].cmp(&right_langs[right_lang_id]) {
-                    Ordering::Less => {
-                        left_lang_id += 1;
-                    }
-                    Ordering::Equal => {
-                        friendship_remove_set.insert(friendship_id);
-
-                        break;
-                    },
-                    Ordering::Greater => {
-                        right_lang_id += 1;
-                    },
-                }
+            if user_langs[first_id].is_disjoint(&user_langs[second_id]) {
+                affected_user_set.insert(first_id);
+                affected_user_set.insert(second_id);
             }
         }
 
-        for (friend_id, friend_value) in friendships.into_iter().enumerate() {
-            if friendship_remove_set.contains(&friend_id) {
-                continue;
-            }
-            let friend_id_a = friend_value[0] as usize - 1;
-            let friend_id_b = friend_value[1] as usize - 1;
-
-            teaching_friend_set.insert(friend_id_a);
-            teaching_friend_set.insert(friend_id_b);
-
-            for lang_id in languages[friend_id_a].iter().chain(languages[friend_id_b].iter()) {
-                teaching_lang_set.insert(*lang_id); 
-            }
-        }
-        let mut min_teaching_count = i32::MAX;
-        
-        for lang_id in teaching_lang_set.iter() {
-            let mut lang_teaching_count: i32 = 0;
-
-            for friend_id in teaching_friend_set.iter() {
-                if languages[*friend_id].binary_search(&lang_id).is_err() {
-                    lang_teaching_count += 1;
-                }
-            }
-
-            min_teaching_count = min_teaching_count.min(lang_teaching_count);
+        if affected_user_set.is_empty() {
+            return 0;
         }
 
-        if min_teaching_count == i32::MAX {
-            0
-        } else {
-            min_teaching_count
+        let mut user_known_list = vec![0usize; (n as usize) + 1];
+
+        for &u in &affected_user_set {
+            for &lang in &user_langs[u] {
+                user_known_list[lang as usize] += 1;
+            }
         }
+        let best = user_known_list.into_iter().max().unwrap_or(0);
+
+        (affected_user_set.len() - best) as i32
+
     }
 }
