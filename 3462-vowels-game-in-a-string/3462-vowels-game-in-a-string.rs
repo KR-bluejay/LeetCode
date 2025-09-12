30338181c6b7a055ef4f5a44c0cf2daf9d3b9488
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-09-12 15:52:28 +0900

    Time: 2 ms (80%), Space: 2.5 MB (10%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.rs
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.rs
@@ -1,10 +1,5 @@
 impl Solution {
     pub fn does_alice_win(s: String) -> bool {
-        s.chars().filter(|v| {
-            match *v {
-                'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U' => true,
-                _ => false
-            }
-        }).count() != 0
+        s.bytes().any(|v| matches!(v | 32, b'a' | b'e' | b'i' | b'o' | b'u'))
     }
 }
