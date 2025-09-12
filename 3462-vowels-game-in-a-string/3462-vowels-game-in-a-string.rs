b138ed28948574ca1dcf5e512c76d9746f581961
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-09-12 15:48:56 +0900

    Time: 3 ms (80%), Space: 2.5 MB (70%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.rs
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.rs
@@ -1,12 +1,10 @@
 impl Solution {
     pub fn does_alice_win(s: String) -> bool {
-        let bowl_count: usize = s.chars().filter(|v| {
+        s.chars().filter(|v| {
             match (*v).to_ascii_lowercase() {
                 'a' | 'e' | 'i' | 'o' | 'u' => true,
                 _ => false
             }
-        }).count();
-
-        bowl_count != 0
+        }).count() != 0
     }
 }
