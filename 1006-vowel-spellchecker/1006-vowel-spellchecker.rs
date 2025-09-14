9e0e43cb946124c3c04563cdf633e93eb1df6450
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-09-14 20:32:53 +0900

    Time: 7 ms (100%), Space: 4.1 MB (100%) - LeetHub

--- 1006-vowel-spellchecker/1006-vowel-spellchecker.rs
+++ 1006-vowel-spellchecker/1006-vowel-spellchecker.rs
@@ -10,19 +10,18 @@ impl Solution {
 
         for word in &wordlist {
             let word = word.as_str();
+            let mut lower_word: String = word.to_ascii_lowercase();
 
             spell_original_set.insert(word);
+            spell_capital_map.entry(lower_word.clone()).or_insert(word);
 
-            let mut lower_word = word.to_ascii_lowercase();
-
-            spell_capital_map.entry(lower_word).or_insert(word);
-
-            let vowel_word: String = word.chars().map(|v| {
-                match v {
-                    'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U' => '*',
-                    _ => v.to_lowercase().next().unwrap()
+            let mut bytes = lower_word.into_bytes();
+            for b in &mut bytes {
+                if matches!(*b, b'a'|b'e'|b'i'|b'o'|b'u') {
+                    *b = b'*';
                 }
-            }).collect();
+            }
+let vowel_word = unsafe { String::from_utf8_unchecked(bytes) };
             spell_vowel_map.entry(vowel_word).or_insert(word);
         }
 
