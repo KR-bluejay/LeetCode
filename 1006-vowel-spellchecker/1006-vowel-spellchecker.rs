18cd2ad8cff436f53b1c747a38a9cd6a2593e8c7
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-09-14 20:25:05 +0900

    Time: 10 ms (100%), Space: 4.1 MB (100%) - LeetHub

--- 1006-vowel-spellchecker/1006-vowel-spellchecker.rs
+++ 1006-vowel-spellchecker/1006-vowel-spellchecker.rs
@@ -2,15 +2,20 @@ use std::collections::{ HashSet, HashMap };
 
 impl Solution {
     pub fn spellchecker(wordlist: Vec<String>, queries: Vec<String>) -> Vec<String> {
-        let mut spell_original_set: HashSet<String> = HashSet::with_capacity(wordlist.len());
+        let mut spell_original_set: HashSet<&str> = HashSet::with_capacity(wordlist.len());
         
-        let mut spell_capital_map: HashMap<String, String> = HashMap::with_capacity(wordlist.len());
-        let mut spell_vowel_map: HashMap<String, String> = HashMap::with_capacity(wordlist.len());
+        let mut spell_capital_map: HashMap<String, &str> = HashMap::with_capacity(wordlist.len());
+        let mut spell_vowel_map: HashMap<String, &str> = HashMap::with_capacity(wordlist.len());
         let mut outputs: Vec<String> = Vec::with_capacity(queries.len());
 
-        for word in wordlist.iter() {
-            spell_original_set.insert(word.clone());
-            spell_capital_map.entry(word.to_lowercase()).or_insert(word.clone());
+        for word in &wordlist {
+            let word = word.as_str();
+
+            spell_original_set.insert(word);
+
+            let mut lower_word = word.to_ascii_lowercase();
+
+            spell_capital_map.entry(lower_word).or_insert(word);
 
             let vowel_word: String = word.chars().map(|v| {
                 match v {
@@ -18,14 +23,14 @@ impl Solution {
                     _ => v.to_lowercase().next().unwrap()
                 }
             }).collect();
-            spell_vowel_map.entry(vowel_word).or_insert(word.clone());
+            spell_vowel_map.entry(vowel_word).or_insert(word);
         }
 
         for query in queries {
-            if let Some(original_word) = spell_original_set.get(&query) {
-                outputs.push(original_word.clone());
-            } else if let Some(capital_word) = spell_capital_map.get(&query.to_lowercase()) {
-                outputs.push(capital_word.clone());
+            if let Some(original_word) = spell_original_set.get(query.as_str()) {
+                outputs.push(original_word.to_string());
+            } else if let Some(capital_word) = spell_capital_map.get(&query.to_ascii_lowercase()) {
+                outputs.push(capital_word.to_string());
             } else {
                 let _vowel_word: String = query.chars().map(|v| {
                     match v {
@@ -35,7 +40,7 @@ impl Solution {
                 }).collect();
 
                 if let Some(vowel_word) = spell_vowel_map.get(&_vowel_word) {
-                    outputs.push(vowel_word.clone());
+                    outputs.push(vowel_word.to_string());
                 } else {
                     outputs.push("".to_string());
                 }
