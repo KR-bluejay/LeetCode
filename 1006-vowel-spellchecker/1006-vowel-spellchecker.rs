3a08b3310e537e2de6e90da086fb416ad0a6d171
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-09-14 20:14:56 +0900

    Time: 11 ms (100%), Space: 4.7 MB (100%) - LeetHub

--- 1006-vowel-spellchecker/1006-vowel-spellchecker.rs
+++ 1006-vowel-spellchecker/1006-vowel-spellchecker.rs
@@ -1,89 +1,46 @@
-use std::collections::HashMap;
+use std::collections::{ HashSet, HashMap };
 
 impl Solution {
     pub fn spellchecker(wordlist: Vec<String>, queries: Vec<String>) -> Vec<String> {
-        let mut word_map: HashMap<String, Vec<usize>> = HashMap::with_capacity(wordlist.len());
+        let mut spell_original_set: HashSet<String> = HashSet::with_capacity(wordlist.len());
+        
+        let mut spell_capital_map: HashMap<String, String> = HashMap::with_capacity(wordlist.len());
+        let mut spell_vowel_map: HashMap<String, String> = HashMap::with_capacity(wordlist.len());
         let mut outputs: Vec<String> = Vec::with_capacity(queries.len());
 
-        for (word_id, word) in wordlist.iter().enumerate() {
-            let word: String = word.chars().filter_map(|v| {
+        for word in wordlist.iter() {
+            spell_original_set.insert(word.clone());
+            spell_capital_map.entry(word.to_lowercase()).or_insert(word.clone());
+
+            let vowel_word: String = word.chars().map(|v| {
                 match v {
-                    'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U' => None,
-                    _ => Some(v.to_lowercase().next().unwrap())
+                    'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U' => '*',
+                    _ => v.to_lowercase().next().unwrap()
                 }
             }).collect();
-
-            word_map.entry(word).or_insert(vec![]).push(word_id);
+            spell_vowel_map.entry(vowel_word).or_insert(word.clone());
         }
 
-        for (query_id, query) in queries.iter().enumerate() {
-            let lower_query: String = query.chars().filter_map(|v| {
-                match v {
-                    'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U' => None,
-                    _ => Some(v.to_lowercase().next().unwrap())
-                }
-            }).collect();
-
-            if let Some(word_ids) = word_map.get(&lower_query) {
-                let mut min_id = usize::MAX;
-                let mut like_id = usize::MAX;
-
-                for word_id in word_ids {
-                    if query.len() != wordlist[*word_id].len() {
-                        continue;
-                    }
-
-                    if *query == wordlist[*word_id] {
-                        outputs.push(wordlist[*word_id].clone());
-                        
-                        break;
-                    } else {
-                        if query.to_lowercase() == wordlist[*word_id].to_lowercase() {
-                            min_id = min_id.min(*word_id);
-                        } else {
-                            for (word_char, query_char) in query.chars().zip(wordlist[*word_id].chars()) {
-                                if word_char.to_lowercase().next().unwrap() == query_char.to_lowercase().next().unwrap() {
-                                    like_id = like_id.min(*word_id);
-
-                                    continue;
-                                }
-
-                                match query_char {
-                                    'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U' => {
-                                        like_id = like_id.min(*word_id);
-                                    },
-                                    _ => {
-                                        println!("{query_id} {word_char} {query_char}");
-                                        println!("{:?} {:?}", query.chars(), wordlist[*word_id].chars());
-                                        println!("");
-
-                                        like_id = if like_id < *word_id { 
-                                            like_id
-                                        } else {
-                                            usize::MAX
-                                        };
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if outputs.len() != query_id + 1 {
-                    if min_id != usize::MAX {
-                        outputs.push(wordlist[min_id].clone());
-                    } else if like_id != usize::MAX {
-                        outputs.push(wordlist[like_id].clone());
-                    } else {
-                        outputs.push("".to_string());
-                    }
-                }
+        for query in queries {
+            if let Some(original_word) = spell_original_set.get(&query) {
+                outputs.push(original_word.clone());
+            } else if let Some(capital_word) = spell_capital_map.get(&query.to_lowercase()) {
+                outputs.push(capital_word.clone());
             } else {
-                outputs.push("".to_string());
+                let _vowel_word: String = query.chars().map(|v| {
+                    match v {
+                        'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U' => '*',
+                        _ => v.to_lowercase().next().unwrap()
+                    }
+                }).collect();
+
+                if let Some(vowel_word) = spell_vowel_map.get(&_vowel_word) {
+                    outputs.push(vowel_word.clone());
+                } else {
+                    outputs.push("".to_string());
+                }
             }
         }
-
 
         outputs
     }
