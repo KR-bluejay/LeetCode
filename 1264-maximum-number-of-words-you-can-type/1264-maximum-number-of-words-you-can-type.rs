651df23c1e07404263d714abf7a88ce2ab993b49
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-09-15 12:12:58 +0900

    Time: 0 ms (100%), Space: 2.2 MB (47.06%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.rs
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.rs
@@ -2,9 +2,11 @@ impl Solution {
     pub fn can_be_typed_words(text: String, broken_letters: String) -> i32 {
         let broken_chars: Vec<char> = broken_letters.chars().collect();
         let mut split_char = text.split_ascii_whitespace();
-        let mut word_count: usize = text.split_ascii_whitespace().count();
+        let mut word_count = 0;
 
         for word in split_char {
+            word_count += 1;
+            
             for broken_char in broken_chars.iter() {
                 if word.contains(*broken_char) {
                     word_count -= 1;
