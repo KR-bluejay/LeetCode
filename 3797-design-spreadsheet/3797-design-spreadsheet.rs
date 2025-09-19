048d5eed47133e5336f1581c1da1671da58b23dc
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-09-19 12:03:03 +0900

    Time: 34 ms (25%), Space: 7.6 MB (25%) - LeetHub

--- 3797-design-spreadsheet/3797-design-spreadsheet.rs
+++ 3797-design-spreadsheet/3797-design-spreadsheet.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+
 struct Spreadsheet {
-    sheet: Vec<Vec<i32>>,
+    cells: HashMap<String, i32>,
 }
 
 
@@ -10,58 +12,36 @@ struct Spreadsheet {
 impl Spreadsheet {
 
     fn new(rows: i32) -> Self {
-        let rows = rows as usize;
-        let sheet: Vec<Vec<i32>> = vec![vec![0; 26]; rows + 1];
-
         Self {
-            sheet
+            cells: HashMap::with_capacity(rows as usize * 26),
         }
     }
-
-    fn get_cell_id(&self, cell: &str) -> (usize, usize) {
-        let row_id: usize = cell[1 ..].parse::<usize>().unwrap() - 1;
-        let cell_id = cell.chars().nth(0).unwrap() as usize - 'A' as usize;
-
-        (row_id, cell_id)
-    }
+    
     fn set_cell(&mut self, cell: String, value: i32) {
-        let mut express_num: i32 = 0;
-        let mut is_right = false;
-        let cells: Vec<&str> = cell.split('+').collect();
-
-        let (row_id, col_id) = self.get_cell_id(cells[0]);
-        let express_num = cells.get(1).unwrap_or(&"0").parse::<i32>().unwrap();
-
-
-        self.sheet[row_id][col_id] = value + express_num;
+        self.cells.insert(cell, value);
     }
     
     fn reset_cell(&mut self, cell: String) {
-        let (row_id, col_id) = self.get_cell_id(&cell);
+        self.cells.insert(cell, 0);
 
-        self.sheet[row_id][col_id] = 0;
     }
-
     
     fn get_value(&self, formula: String) -> i32 {
-        let mut value: i32 = 0;
-        let formula: String = formula[1..].to_string();
-        let formulas: Vec<&str> = formula.split('+').collect();
-        
+        let mut value = 0;
 
-        for formula_item in formulas {
+        for formula_item in formula[1..].split('+') {
             match formula_item.parse::<i32>() {
                 Ok(num) => {
                     value += num;
                 }
-                Err(e) => {
-                    let (row_id, cell_id) = self.get_cell_id(&formula_item);
-                    value += self.sheet[row_id][cell_id];
+                _ => {
+                    value += self.cells.get(formula_item).unwrap_or(&0);
                 }
             }
         }
 
         value
+
     }
 }
 
