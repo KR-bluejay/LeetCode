3f88fbf7dd9eb60b311db37502e4c34500bb7c97
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-09-23 11:25:57 +0900

    Time: 0 ms (100%), Space: 2.2 MB (71.43%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.rs
+++ 0165-compare-version-numbers/0165-compare-version-numbers.rs
@@ -9,13 +9,7 @@ impl Solution {
             .map(|v| v.parse::<i32>().unwrap())
             .collect();
 
-        match version1[0].cmp(&version2[0]) {
-            Ordering::Less => return -1,
-            Ordering::Greater => return 1,
-            _ => {}
-        };
-
-        for i in 1 .. version1.len().max(version2.len()) {
+        for i in 0 .. version1.len().max(version2.len()) {
             let version1_tmp = *version1.get(i).unwrap_or(&0);
             let version2_tmp = *version2.get(i).unwrap_or(&0);
 
