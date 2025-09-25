16cfb64bce0cfed945d7dcf35d3bb86d1b601b8e
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-09-25 14:12:22 +0900

    Time: 79 ms (2.7%), Space: 2.4 MB (43.24%) - LeetHub

--- 0120-triangle/0120-triangle.rs
+++ 0120-triangle/0120-triangle.rs
@@ -1,11 +1,35 @@
 impl Solution {
-    pub fn minimum_total(mut triangle: Vec<Vec<i32>>) -> i32 {
-        for i in (0 .. triangle.len() - 1).rev() {
-            for j in (0 .. triangle[i].len()) {
-                triangle[i][j] += triangle[i + 1][j].min(triangle[i + 1][j + 1]);
-            }
+    fn short_path(
+        triangle: &Vec<Vec<i32>>, 
+        row_id: usize, 
+        col_id: usize, 
+        prev_path: i32, 
+        min_path: &mut i32,
+        visited: &mut Vec<Vec<i32>>
+    ) {
+        if row_id >= triangle.len() || triangle[row_id].len() <= col_id || visited[row_id][col_id] <= prev_path +  triangle[row_id][col_id] {
+            return;
         }
 
-        triangle[0][0]
+        let cur_path = prev_path + triangle[row_id][col_id];
+        visited[row_id][col_id] = cur_path;
+
+        if row_id == triangle.len() - 1 {
+            *min_path = cur_path.min(*min_path);
+            
+            return;
+        }
+
+        // Self::short_path(triangle, row_id + 1, col_id - 1, cur_path, min_path);
+        Self::short_path(triangle, row_id + 1, col_id, cur_path, min_path, visited);
+        Self::short_path(triangle, row_id + 1, col_id + 1, cur_path, min_path, visited);
+    }
+    pub fn minimum_total(triangle: Vec<Vec<i32>>) -> i32 {
+        let mut min_path = i32::MAX;
+        let mut visited: Vec<Vec<i32>> = vec![vec![i32::MAX; triangle[triangle.len() - 1].len()]; triangle.len()];
+
+        Self::short_path(&triangle, 0, 0, 0, &mut min_path, &mut visited);
+
+        min_path
     }
 }
