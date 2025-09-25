15cd679e0ca0deea09bdb51b8e88ed45912ed84b
KR-bluejay <145690647+KR-bluejay@users.noreply.github.com>
2025-09-25 14:18:45 +0900

    Time: 0 ms (100%), Space: 2.5 MB (16.22%) - LeetHub

--- 0120-triangle/0120-triangle.rs
+++ 0120-triangle/0120-triangle.rs
@@ -1,35 +1,29 @@
 impl Solution {
-    fn short_path(
-        triangle: &Vec<Vec<i32>>, 
-        row_id: usize, 
-        col_id: usize, 
-        prev_path: i32, 
-        min_path: &mut i32,
-        visited: &mut Vec<Vec<i32>>
-    ) {
-        if row_id >= triangle.len() || triangle[row_id].len() <= col_id || visited[row_id][col_id] <= prev_path +  triangle[row_id][col_id] {
-            return;
+    fn shortest_path(
+        triangle: &Vec<Vec<i32>>,
+        cache: &mut Vec<Vec<i32>>,
+        row_id: usize,
+        col_id: usize,
+    ) -> i32 {
+        if triangle.len() <= row_id 
+        || triangle[row_id].len() <= col_id {
+            return 0;
         }
 
-        let cur_path = prev_path + triangle[row_id][col_id];
-        visited[row_id][col_id] = cur_path;
-
-        if row_id == triangle.len() - 1 {
-            *min_path = cur_path.min(*min_path);
-            
-            return;
+        if cache[row_id][col_id] != i32::MAX {
+            return cache[row_id][col_id];
         }
+        let path_a = Self::shortest_path(triangle, cache, row_id + 1, col_id);
+        let path_b = Self::shortest_path(triangle, cache, row_id + 1, col_id + 1);
 
-        // Self::short_path(triangle, row_id + 1, col_id - 1, cur_path, min_path);
-        Self::short_path(triangle, row_id + 1, col_id, cur_path, min_path, visited);
-        Self::short_path(triangle, row_id + 1, col_id + 1, cur_path, min_path, visited);
+
+        cache[row_id][col_id] = triangle[row_id][col_id] + path_a.min(path_b);
+
+        cache[row_id][col_id]
     }
     pub fn minimum_total(triangle: Vec<Vec<i32>>) -> i32 {
-        let mut min_path = i32::MAX;
-        let mut visited: Vec<Vec<i32>> = vec![vec![i32::MAX; triangle[triangle.len() - 1].len()]; triangle.len()];
+        let mut cache: Vec<Vec<i32>> = vec![vec![i32::MAX; triangle[triangle.len() - 1].len()]; triangle.len()];
 
-        Self::short_path(&triangle, 0, 0, 0, &mut min_path, &mut visited);
-
-        min_path
+        Self::shortest_path(&triangle, &mut cache, 0, 0)
     }
 }
