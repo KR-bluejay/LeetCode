cdd631c9e6376693658737ae11fd94808b49b73c
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-10-16 12:26:18 +0900

    Time: 0 ms (100%), Space: 3.8 MB (50%) - LeetHub

--- 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.rs
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.rs
@@ -1,35 +1,16 @@
-use std::collections::{ HashMap, BTreeSet };
-
 impl Solution {
     pub fn find_smallest_integer(nums: Vec<i32>, value: i32) -> i32 {
-        let mut modulo_map: HashMap<i32, i32> = HashMap::with_capacity(nums.len());
-        let mut num_set: BTreeSet<i32> = BTreeSet::new();
+        let mut min_rem = 0;
+        let mut num_counts = vec![0; value as usize];
 
-        for num in nums.iter() {
-            modulo_map.entry((num % value).rem_euclid(value))
-                .and_modify(|v| *v += 1)
-                .or_insert(0);
+        for &num in nums.iter() {
+            num_counts[(num % value).rem_euclid(value) as usize] += 1;
         }
 
-        for (k, &v) in modulo_map.iter() {
-            for i in 0 ..= v {
-                num_set.insert(k + value * i);
-            }
+        while num_counts[(min_rem % value) as usize] > 0 {
+            num_counts[(min_rem % value) as usize] -= 1;
+            min_rem += 1;
         }
-
-
-        let mut prev_num = *num_set.first().unwrap();
-
-        if prev_num != 0 {
-            return 0;
-        }
-
-        for &num in num_set.iter().skip(1) {
-            if prev_num + 1 != num {
-                return prev_num + 1;
-            }
-            prev_num = num;
-        }
-        prev_num + 1
+        min_rem
     }
 }
