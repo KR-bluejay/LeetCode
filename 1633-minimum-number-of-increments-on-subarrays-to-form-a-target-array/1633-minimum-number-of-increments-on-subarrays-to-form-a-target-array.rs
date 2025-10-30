206753e6e9f9bd220d08700338fdfc7a8ac5d56e
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-10-30 12:57:08 +0900

    Time: 0 ms (100%), Space: 3.3 MB (40%) - LeetHub

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.rs
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.rs
@@ -3,15 +3,13 @@ use std::collections::VecDeque;
 impl Solution {
     pub fn min_number_operations(target: Vec<i32>) -> i32 {
         let mut op_count: i32 = target[0];
-        let mut cur_num: i32 = target[0];
+        let mut prev_num: i32 = target[0];
 
-        for &item in target.iter().skip(1) {
-            cur_num = cur_num.min(item);
-
-            if cur_num < item {
-                op_count += item - cur_num;
-                cur_num = item;
+        for &cur_num in target.iter().skip(1) {
+            if prev_num < cur_num {
+                op_count += cur_num - prev_num;
             }
+            prev_num = cur_num;
         }
         
         op_count
