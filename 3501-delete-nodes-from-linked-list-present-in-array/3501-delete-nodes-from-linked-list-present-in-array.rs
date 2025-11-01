2b1aaa49e3b7df9023ca256cd05b19e1b68b91f5
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-01 19:02:17 +0900

    Time: 15 ms (80%), Space: 10.7 MB (20%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.rs
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.rs
@@ -1,4 +1,5 @@
-use std::collections::{HashSet};
+use std::collections::HashSet;
+
 // Definition for singly-linked list.
 // #[derive(PartialEq, Eq, Clone, Debug)]
 // pub struct ListNode {
@@ -22,21 +23,21 @@ impl Solution {
     ) -> Option<Box<ListNode>> {
         let num_set: HashSet<i32> = nums.into_iter().collect();
 
-        let mut cur_node = &mut head;
-
-        loop {
-            match cur_node {
-                Some(node) if num_set.contains(&node.val) => {
-                    let next_node = node.next.take();
-                    
-                    *cur_node = next_node;
-                },
-                Some(node) => {
-                    cur_node = &mut node.next;
-                }
-                None => break,
-            }
+        while matches!(head.as_ref(), Some(node) if num_set.contains(&node.val)) {
+            head = head.take()?.next;
         }
+
+        let mut cur_node = head.as_mut();
+
+        while let Some(node) = cur_node {
+            while matches!(
+                node.next.as_ref(), Some(next_node) if num_set.contains(&next_node.val)
+            ) {
+                node.next = node.next.take()?.next;
+            }
+            cur_node = node.next.as_mut();
+        }
+
         head
     }
 }
