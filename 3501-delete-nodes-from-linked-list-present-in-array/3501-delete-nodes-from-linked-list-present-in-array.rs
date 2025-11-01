a8c0e560f774adf2324dd2f0b446f9a8f2302983
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-01 18:41:59 +0900

    Time: 26 ms (20%), Space: 10.8 MB (20%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.rs
@@ -0,0 +1,40 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn modified_list(
+        mut nums: Vec<i32>, 
+        mut head: Option<Box<ListNode>>
+    ) -> Option<Box<ListNode>> {
+        nums.sort();
+
+        let mut cur_node = &mut head;
+
+        loop {
+            match cur_node {
+                Some(node) if nums.binary_search(&node.val).is_ok() => {
+                    let next_node = node.next.take();
+                    *cur_node = next_node;
+                },
+                Some(node) => {
+                    cur_node = &mut node.next;
+                }
+                None => break,
+            }
+        }
+        head
+    }
+}
