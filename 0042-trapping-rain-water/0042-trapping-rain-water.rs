48ab96caf748f612fd1a7c298098c88918da5562
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-02 19:48:19 +0900

    Time: 0 ms (100%), Space: 2.2 MB (97.77%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.rs
+++ 0042-trapping-rain-water/0042-trapping-rain-water.rs
@@ -1,21 +1,30 @@
 impl Solution {
     pub fn trap(heights: Vec<i32>) -> i32 {
-        let (mut left_id, mut right_id) = (0, heights.len() - 1);
-        let (mut left_max, mut right_max) = (0, 0);
-        let mut trapped_water = 0;
+        let mut water_volume = 0;
+        let mut stack_ids: Vec<usize> = Vec::with_capacity(heights.len());
 
-        while left_id < right_id {
-            left_max = left_max.max(heights[left_id]);
-            right_max = right_max.max(heights[right_id]);
+        for (right_id, &right_val) in heights.iter().enumerate() {
+            let mut prev_height = 0;
 
-            if left_max <= right_max {
-                trapped_water += left_max - heights[left_id];
-                left_id += 1;
-            } else {
-                trapped_water += right_max - heights[right_id];
-                right_id -= 1;
+            while let Some(&left_id) = stack_ids.last() 
+            && heights[left_id] <= right_val {
+                let width = (right_id - left_id) as i32 - 1;
+                let height = heights[left_id] - prev_height;
+
+                water_volume += width * height;
+                prev_height = heights[left_id];
+                
+                stack_ids.pop();
             }
+
+            if 0 < stack_ids.len() && prev_height < right_val {
+                let width = (right_id - stack_ids.last().unwrap()) as i32 - 1;
+                let height = right_val - prev_height;
+
+                water_volume += width * height;
+            }
+            stack_ids.push(right_id);
         }
-        trapped_water
+        water_volume
     }
 }
