d7250d8c87e9a0e1c787d4cb1b5f4c42d61ff1d5
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-04 16:13:59 +0900

    Time: 0 ms (100%), Space: 2.3 MB (25%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.rs
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.rs
@@ -1,41 +1,70 @@
-use std::collections::{BTreeMap};
+use std::collections::BTreeSet;
 
 impl Solution {
     pub fn find_x_sum(nums: Vec<i32>, k: i32, x: i32) -> Vec<i32> {
         let k = k as usize;
+        let x = x as usize;
+        let n = nums.len();
 
-        let mut results: Vec<i32> = Vec::with_capacity(nums.len() - k);
+        let mut freq = [0_i32; 51];
 
-        let mut num_map: BTreeMap<i32, i32> = nums[0 .. k - 1]
-            .iter()
-            .fold(BTreeMap::new(), |mut acc, &item| {
-                *acc.entry(item )
-                    .and_modify(|v| *v += 1)
-                    .or_insert(1);
-                acc
-            });
-        
-        for (num_id, &num_val) in nums.iter().enumerate().skip(k - 1){
-            if k <= num_id {
-                num_map.entry(nums[num_id - k] ).and_modify(|v| *v -= 1);
-            }
-
-            num_map.entry(num_val)
-                .and_modify(|v| *v += 1)
-                .or_insert(1);
-
-            let mut temp = 0;
-
-            let mut test: Vec<(i32, i32)> = num_map.clone().into_iter().map(|(k, v)| (k, v)).collect();
-            test.sort_by(|a, b| b.1.cmp(&a.1).then_with(|| b.0.cmp(&a.0)));
-            let mut temp = 0;
-            for item in test.into_iter().take(x as usize) {
-                temp += item.0 * item.1;
-            }
-            results.push(temp);
-
+        for &v in nums.iter().take(k) {
+            freq[v as usize] += 1;
         }
 
-        results
+        let mut set: BTreeSet<(i32, i32)> = BTreeSet::new();
+        for value in 1..=50 {
+            let f = freq[value];
+            if f > 0 {
+                set.insert((-f, -(value as i32)));
+            }
+        }
+
+        fn current_x_sum(set: &BTreeSet<(i32, i32)>, x: usize) -> i32 {
+            let mut sum = 0;
+            for &(neg_f, neg_v) in set.iter().take(x) {
+                let f = -neg_f;
+                let v = -neg_v;
+                sum += f * v;
+            }
+            sum
+        }
+
+        let mut ans = Vec::with_capacity(n - k + 1);
+
+        ans.push(current_x_sum(&set, x));
+
+        for i in k..n {
+            let out = nums[i - k] as usize;
+            let inn = nums[i] as usize;
+
+            {
+                let old_f = freq[out];
+                if old_f > 0 {
+                    set.remove(&(-old_f, -(out as i32)));
+                }
+                freq[out] -= 1;
+                let new_f = freq[out];
+                if new_f > 0 {
+                    set.insert((-new_f, -(out as i32)));
+                }
+            }
+
+            {
+                let old_f = freq[inn];
+                if old_f > 0 {
+                    set.remove(&(-old_f, -(inn as i32)));
+                }
+                freq[inn] += 1;
+                let new_f = freq[inn];
+                if new_f > 0 {
+                    set.insert((-new_f, -(inn as i32)));
+                }
+            }
+
+            ans.push(current_x_sum(&set, x));
+        }
+
+        ans
     }
 }
