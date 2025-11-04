42a802d763ae84bd8ac3b706c5f48d6017c4423a
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-04 22:00:09 +0900

    Time: 7 ms (68.6%), Space: 2.7 MB (55.23%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.rs
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.rs
@@ -1,26 +1,7 @@
-use std::collections::{BinaryHeap, HashSet};
-use std::cmp::Ordering;
+use std::collections::{BinaryHeap};
+use std::cmp::Reverse;
 
-#[derive(Eq, PartialEq)]
-struct Block {
-    row_id: usize,
-    col_id: usize,
-    height: i32
-}
 
-impl Ord for Block {
-    #[inline(always)]
-    fn cmp(&self, other: &Self) -> Ordering {
-        other.height.cmp(&self.height)
-    }
-}
-
-impl PartialOrd for Block {
-    #[inline(always)]
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(&other))
-    }
-}
 
 impl Solution {
     #[inline(always)]
@@ -30,45 +11,44 @@ impl Solution {
 
         let mut water_amount = 0;
 
-        let mut block_queue: BinaryHeap<Block> 
+        let mut block_queue: BinaryHeap<Reverse<(i32, usize, usize)>> 
             = BinaryHeap::with_capacity((max_row_id + 1) * (max_col_id + 1));
         let mut block_visit: Vec<Vec<bool>> = vec![vec![false; max_col_id + 1]; max_row_id + 1];
 
         for row_id in 0 ..= max_row_id {
             block_visit[row_id][0] = true;
-            block_queue.push(Block {
+            block_queue.push(Reverse((
+                height_map[row_id][0],
                 row_id,
-                col_id: 0,
-                height: height_map[row_id][0]
-            });
+                0
+            )));
 
             block_visit[row_id][max_col_id] = true;
-            block_queue.push(Block {
+            block_queue.push(Reverse((
+                height_map[row_id][max_col_id],
                 row_id,
-                col_id: max_col_id,
-                height: height_map[row_id][max_col_id]
-            });
+                max_col_id
+            )));
         }
 
         for col_id in 1 .. max_col_id {
             block_visit[0][col_id] = true;
-            block_queue.push(Block {
-                row_id: 0,
-                col_id,
-                height: height_map[0][col_id]
-            });
+            block_queue.push(Reverse((
+                height_map[0][col_id],
+                0,
+                col_id
+            )));
 
             block_visit[max_row_id][col_id] = true;
-            block_queue.push(Block {
-                row_id: max_row_id,
-                col_id,
-                height: height_map[max_row_id][col_id]
-            });
+            block_queue.push(Reverse((
+                height_map[max_row_id][col_id],
+                max_row_id,
+                col_id
+            )));
         }
 
 
-        while let Some(block) = block_queue.pop() {
-            let Block { row_id, col_id, height } = block;
+        while let Some(Reverse((height, row_id, col_id))) = block_queue.pop() {
             let next_pos = [
                 (row_id, col_id.wrapping_sub(1)), 
                 (row_id, col_id + 1), 
@@ -87,11 +67,11 @@ impl Solution {
                 water_amount += (height - next_height).max(0);
 
                 block_visit[next_row_id][next_col_id] = true;
-                block_queue.push(Block {
-                    row_id: next_row_id,
-                    col_id: next_col_id,
-                    height: height.max(next_height)
-                });
+                block_queue.push(Reverse ((
+                    height.max(next_height),
+                    next_row_id,
+                    next_col_id,
+                )));
             }
         }
 
