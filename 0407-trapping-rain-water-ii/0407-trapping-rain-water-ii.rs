6f0e771f1de53b879f6a8124041f2c80639a495a
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-04 22:04:36 +0900

    Time: 3 ms (97.38%), Space: 3 MB (25.87%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.rs
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.rs
@@ -1,81 +1,65 @@
-use std::collections::{BinaryHeap};
-use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use std::cmp::Ordering;
 
+#[derive(Eq, PartialEq, Debug)]
+struct BlockTuple(i32, usize, usize);
 
+impl Ord for BlockTuple {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.0.cmp(&self.0)
+    }
+}
+
+impl PartialOrd for BlockTuple {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 impl Solution {
-    #[inline(always)]
-    pub fn trap_rain_water(mut height_map: Vec<Vec<i32>>) -> i32 {
-        let max_row_id = height_map.len() - 1;
-        let max_col_id = height_map[0].len() - 1;
+    pub fn trap_rain_water(height_map: Vec<Vec<i32>>) -> i32 {
+        if height_map.is_empty() || height_map[0].is_empty() { return 0; }
 
+        let m = height_map.len();
+        let n = height_map[0].len();
         let mut water_amount = 0;
 
-        let mut block_queue: BinaryHeap<Reverse<(i32, usize, usize)>> 
-            = BinaryHeap::with_capacity((max_row_id + 1) * (max_col_id + 1));
-        let mut block_visit: Vec<Vec<bool>> = vec![vec![false; max_col_id + 1]; max_row_id + 1];
+        let mut heap: BinaryHeap<BlockTuple> = BinaryHeap::with_capacity(m * n);
+        let mut visited = vec![vec![false; n]; m];
 
-        for row_id in 0 ..= max_row_id {
-            block_visit[row_id][0] = true;
-            block_queue.push(Reverse((
-                height_map[row_id][0],
-                row_id,
-                0
-            )));
-
-            block_visit[row_id][max_col_id] = true;
-            block_queue.push(Reverse((
-                height_map[row_id][max_col_id],
-                row_id,
-                max_col_id
-            )));
+        for row in 0..m {
+            visited[row][0] = true;
+            visited[row][n-1] = true;
+            heap.push(BlockTuple(height_map[row][0], row, 0));
+            heap.push(BlockTuple(height_map[row][n-1], row, n-1));
+        }
+        for col in 1..n-1 {
+            visited[0][col] = true;
+            visited[m-1][col] = true;
+            heap.push(BlockTuple(height_map[0][col], 0, col));
+            heap.push(BlockTuple(height_map[m-1][col], m-1, col));
         }
 
-        for col_id in 1 .. max_col_id {
-            block_visit[0][col_id] = true;
-            block_queue.push(Reverse((
-                height_map[0][col_id],
-                0,
-                col_id
-            )));
+        let directions: [(i32, i32); 4] = [(-1,0),(1,0),(0,-1),(0,1)];
 
-            block_visit[max_row_id][col_id] = true;
-            block_queue.push(Reverse((
-                height_map[max_row_id][col_id],
-                max_row_id,
-                col_id
-            )));
-        }
+        while let Some(BlockTuple(height, row, col)) = heap.pop() {
+            for &(dr, dc) in &directions {
+                let nr = row as i32 + dr;
+                let nc = col as i32 + dc;
 
+                if nr < 0 || nr >= m as i32 || nc < 0 || nc >= n as i32 { continue; }
+                let nr = nr as usize;
+                let nc = nc as usize;
 
-        while let Some(Reverse((height, row_id, col_id))) = block_queue.pop() {
-            let next_pos = [
-                (row_id, col_id.wrapping_sub(1)), 
-                (row_id, col_id + 1), 
-                (row_id.wrapping_sub(1), col_id), 
-                (row_id + 1, col_id)
-            ];
+                if visited[nr][nc] { continue; }
 
-            for (&(next_row_id, next_col_id))in next_pos.iter() {
-                if max_row_id < next_row_id 
-                || max_col_id < next_col_id 
-                || block_visit[next_row_id][next_col_id] {
-                    continue;
-                }
-                let next_height = height_map[next_row_id][next_col_id];
-
-                water_amount += (height - next_height).max(0);
-
-                block_visit[next_row_id][next_col_id] = true;
-                block_queue.push(Reverse ((
-                    height.max(next_height),
-                    next_row_id,
-                    next_col_id,
-                )));
+                visited[nr][nc] = true;
+                let nh = height_map[nr][nc];
+                water_amount += (height - nh).max(0);
+                heap.push(BlockTuple(height.max(nh), nr, nc));
             }
         }
 
         water_amount
-
     }
 }
