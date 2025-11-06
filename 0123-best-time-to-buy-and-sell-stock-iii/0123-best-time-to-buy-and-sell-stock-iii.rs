a82c804f979d3d459380fe3de9fa52f18b6ff238
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-06 21:10:57 +0900

    Time: 0 ms (100%), Space: 3.3 MB (29.55%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.rs
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.rs
@@ -1,20 +1,15 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        let mut buy_1 = i32::MIN;
-        let mut sell_2 = i32::MIN / 2;
-        let mut buy_3 = i32::MIN / 2;
-        let mut sell_4 = i32::MIN / 2;
+        let mut buy_1 = -100000;
+        let mut sell_2 = -100000;
+        let mut buy_3 = -100000;
+        let mut sell_4 = -100000;
 
         for &price in prices.iter() {
-            let cur_buy_1 = buy_1.max(0 - price);
-            let cur_sell_2 = sell_2.max(buy_1 + price);
-            let cur_buy_3 = buy_3.max(sell_2 - price);
-            let cur_sell_4 = sell_4.max(buy_3 + price);
-
-            buy_1 = cur_buy_1;
-            sell_2 = cur_sell_2;
-            buy_3 = cur_buy_3;
-            sell_4 = cur_sell_4;
+            sell_4 = sell_4.max(buy_3 + price);
+            buy_3 = buy_3.max(sell_2 - price);
+            sell_2 = sell_2.max(buy_1 + price);
+            buy_1 = buy_1.max(0 - price);
         }
 
 
