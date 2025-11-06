9f26595b82cbf68a7d0c299880071f714f0cc1a7
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-07 01:12:07 +0900

    Time: 0 ms (100%), Space: 2.4 MB (98.28%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.rs
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.rs
@@ -4,7 +4,7 @@
 //   pub val: i32,
 //   pub next: Option<Box<ListNode>>
 // }
-// 
+//
 // impl ListNode {
 //   #[inline]
 //   fn new(val: i32) -> Self {
@@ -14,51 +14,52 @@
 //     }
 //   }
 // }
-use itertools::Itertools;
 
 impl Solution {
-    pub fn reverse_k_group(
-        head: Option<Box<ListNode>>, 
-        k: i32
+    #[inline]
+    fn connect_tail(
+        mut node: Option<Box<ListNode>>,
+        rest: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
-        let k = k as usize;
-
-        let mut nums: Vec<i32> = Vec::with_capacity(5000);
-        let mut opt_node = head;
-
-        while let Some(mut node) = opt_node {
-            nums.push(node.val);
-            opt_node = node.next.take();
-        }
-
-        let mut dummy_node = Some(Box::new(ListNode {
-            val: 0,
-            next: None,
-        }));
-        let mut prev_node = &mut dummy_node;
-
-
-        for num_chunk in nums.chunks(k) {
-            if num_chunk.len() < k {
-                for &num in num_chunk.iter() {
-                    prev_node.as_mut().unwrap().next = Some(Box::new(ListNode {
-                        val: num,
-                        next: None,
-                    }));
-                    prev_node = &mut prev_node.as_mut().unwrap().next;
-                }
+        if let Some(ref mut n) = node {
+            if n.next.is_some() {
+                n.next = Self::connect_tail(n.next.take(), rest);
             } else {
-                for &num in num_chunk.iter().rev() {
-                    prev_node.as_mut().unwrap().next = Some(Box::new(ListNode {
-                        val: num,
-                        next: None,
-                    }));
-                    prev_node = &mut prev_node.as_mut().unwrap().next;
-                }
+                n.next = rest;
             }
         }
-
-
-        dummy_node.unwrap().next
+        node
+    }
+    pub fn reverse_k_group(
+        head: Option<Box<ListNode>>,
+        k: i32,
+    ) -> Option<Box<ListNode>> {
+        let k = k as usize;
+        
+        let mut check = &head;
+        for _ in 0..k {
+            match check {
+                Some(node) => check = &node.next,
+                None => return head,
+            }
+        }
+        
+        let mut prev = None;
+        let mut curr = head;
+        
+        for _ in 0..k {
+            curr = match curr {
+                Some(mut node) => {
+                    let next = node.next.take();
+                    node.next = prev;
+                    prev = Some(node);
+                    next
+                }
+                None => break,
+            };
+        }
+        
+        
+        Self::connect_tail(prev, Self::reverse_k_group(curr, k as i32))
     }
 }
