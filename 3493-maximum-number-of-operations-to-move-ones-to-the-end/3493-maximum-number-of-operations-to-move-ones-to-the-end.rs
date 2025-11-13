9b48f4d68dacf3c8ffd259760643fb237eb41530
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-13 09:58:11 +0900

    Time: 4 ms (66.67%), Space: 2.4 MB (100%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.rs
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.rs
@@ -3,21 +3,19 @@ impl Solution {
         let mut bytes: Vec<u8> = s.into_bytes().into_iter().map(|v| v - b'0').collect();
         let mut op_count = 0;
 
-        let mut one_count = 0;
+        let mut one_count = bytes[0] as i32;
 
         let mut is_add = false;
 
-        for id in 0 .. bytes.len() {
+        for id in 1 .. bytes.len() {
             if bytes[id] == 1 {
                 one_count += 1;
-                is_add = true;
-            } else if is_add {
+            } else if bytes[id - 1] == 1 {
                 op_count += one_count;
-                is_add = false;
             }
         }
 
 
-        op_count as i32
+        op_count
     }
 }
