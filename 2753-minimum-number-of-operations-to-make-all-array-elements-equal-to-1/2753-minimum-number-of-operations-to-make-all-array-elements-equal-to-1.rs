0fc742f8e305bfba16bd1f9b8ae309c3b893fe2d
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-13 11:22:18 +0900

    Time: 0 ms (100%), Space: 2.2 MB (60%) - LeetHub

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.rs
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.rs
@@ -13,22 +13,19 @@ impl Solution {
     }
     pub fn min_operations(nums: Vec<i32>) -> i32 {
         let mut op_count = usize::MAX;
-        let mut one_count = 0;
 
         for i in 0 .. nums.len() - 1 {
             let mut lhs = nums[i];
 
             if lhs == 1 {
-                one_count += 1;
-
-                continue;
+                return nums.len() as i32  - nums.iter().skip(i).filter(|v| **v == 1).count() as i32;
             }
 
             for j in i + 1 .. nums.len() {
                 let mut rhs = nums[j];
 
                 if rhs == 1 {
-                    continue;
+                    return nums.len() as i32  - nums.iter().skip(j).filter(|v| **v == 1).count() as i32;
                 }
 
                 if op_count <= j - i {
@@ -42,16 +39,6 @@ impl Solution {
                     break;
                 }
             }
-        }
-
-        one_count += if nums[nums.len() - 1] == 1 {
-            1
-        } else {
-            0
-        };
-        
-        if one_count > 0 {
-            return nums.len() as i32 - one_count as i32;
         }
 
         if op_count == usize::MAX {
