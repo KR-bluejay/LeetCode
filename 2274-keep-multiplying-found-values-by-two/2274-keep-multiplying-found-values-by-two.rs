0d8532db31c69bbd97eec01d9109a560c2ed149e
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-19 16:06:17 +0900

    Time: 0 ms (100%), Space: 2.1 MB (72.73%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.rs
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.rs
@@ -1,11 +1,14 @@
-use std::collections::HashMap;
-use itertools::Itertools;
+use std::collections::HashSet;
 
 impl Solution {
-    pub fn find_final_value(nums: Vec<i32>, mut original: i32) -> i32 {
-        let num_map = nums.iter().counts();
+    pub fn find_final_value(mut nums: Vec<i32>, mut original: i32) -> i32 {
+        let num_len = nums.len();
+        let num_set: HashSet<i32> = nums.into_iter().fold(HashSet::with_capacity(num_len), |mut acc, num| {
+            acc.insert(num);
+            acc
+        });
 
-        while num_map.get(&original).is_some() {
+        while num_set.get(&original).is_some() {
             original *= 2;
         }
 
