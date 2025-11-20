f085f5cd3527eb988a7f95f11f306cd980e6fce6
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-20 15:33:07 +0900

    Time: 0 ms (100%), Space: 2.3 MB (22.73%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.rs
+++ 0045-jump-game-ii/0045-jump-game-ii.rs
@@ -1,46 +1,25 @@
 impl Solution {
-    fn dp(count: i32, id: usize, nums: &Vec<i32>, jump_counts: &mut Vec<i32>) {
-        if id >= nums.len() {
-            jump_counts[nums.len() - 1] = jump_counts[nums.len() - 1].min(count);
-
-            return;
-        }
-
-        if jump_counts[id] < count || jump_counts[nums.len() - 1] <= count {
-            return;
-        }
-
-        jump_counts[id] = count;
-
-        let mut max_id = id;
-        
-
-        for next_id in (id + 1 ..= id + nums[id] as usize).rev() {
-            let next_count = count + 1;
-
-            if next_id >= nums.len() {
-                jump_counts[nums.len() - 1] = jump_counts[nums.len() - 1].min(next_count);
-
-                continue;
-            }
-
-
-            jump_counts[next_id] = jump_counts[next_id].min(next_count);
-
-            if max_id == id {
-                max_id = next_id;
-            } else if (nums[max_id] + (max_id - next_id) as i32) < nums[next_id] {
-                max_id = next_id;
-            }
-        }
-
-        Self::dp(count + 1, max_id, nums, jump_counts);
-    }
     pub fn jump(nums: Vec<i32>) -> i32 {
-        let mut jump_counts: Vec<i32> = vec![i32::MAX; nums.len()];
+        let mut cur_end = 0;
+        let mut cur_farthest = 0;
 
-        Self::dp(0, 0, &nums, &mut jump_counts);
+        let mut jump = 0;
 
-        *jump_counts.last().unwrap()
+        for (id, &num) in nums.iter().enumerate().take(nums.len() - 1) {
+            let num = num as usize;
+
+            cur_farthest = cur_farthest.max(id + num);
+
+            if id == cur_end {
+                jump += 1;
+                cur_end = cur_farthest;
+
+                if nums.len() - 1 <= cur_end {
+                    break;
+                }
+            }
+        }
+
+        jump
     }
 }
