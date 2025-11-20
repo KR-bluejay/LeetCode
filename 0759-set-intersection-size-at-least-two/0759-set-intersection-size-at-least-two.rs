23e5d919017fe27bf5d9109f43b80821452c3151
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-20 14:37:59 +0900

    Time: 1 ms (100%), Space: 2.5 MB (0%) - LeetHub

--- 0759-set-intersection-size-at-least-two/0759-set-intersection-size-at-least-two.rs
+++ 0759-set-intersection-size-at-least-two/0759-set-intersection-size-at-least-two.rs
@@ -1,6 +1,6 @@
 impl Solution {
     pub fn intersection_size_two(mut intervals: Vec<Vec<i32>>) -> i32 {
-        intervals.sort_by(|left, right| left[1].cmp(&right[1]).then_with(|| right[0].cmp(&left[0])));
+        intervals.sort_unstable_by(|left, right| left[1].cmp(&right[1]).then_with(|| right[0].cmp(&left[0])));
 
         let mut inter_count = 2;
         let mut second_last_num = intervals[0][1] - 1;
