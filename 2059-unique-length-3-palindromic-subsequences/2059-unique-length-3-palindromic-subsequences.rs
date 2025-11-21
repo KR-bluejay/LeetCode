6ee6b3ed328ead6a85f52d8dce23c73a728c5fad
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-21 13:59:18 +0900

    Time: 2 ms (85.71%), Space: 2.5 MB (85.71%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.rs
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.rs
@@ -1,49 +1,41 @@
 impl Solution {
     pub fn count_palindromic_subsequence(s: String) -> i32 {
-        let bytes: Vec<u8> = s.into_bytes();
-        let mut visit: Vec<bool> = vec![false; 26];
-
-        let mut last_ids: Vec<usize> = vec![0; 26];
-
+        let mut bytes: Vec<u8> = s.into_bytes();
+        let mut first_ids = vec![usize::MAX; 26];
+        let mut last_ids = vec![0; 26];
         let mut result = 0;
 
-        for id in (0 .. bytes.len()).rev() {
+        for id in 0 .. bytes.len() {
             let char_id = (bytes[id] - 97) as usize;
-            
-            if last_ids[char_id] == 0 {
-                last_ids[char_id] = id;
+
+            if first_ids[char_id] == usize::MAX {
+                first_ids[char_id] = id;
             }
+            last_ids[char_id] = id;
         }
 
-        for id in (0 .. bytes.len()) {
-            let char_id = (bytes[id] - 97) as usize;
-            let mut tmp_visit: Vec<bool> = vec![false; 26];
+        for id in 0 .. 26 {
             let mut count = 0;
+            let mut visit: u32 = 0;
 
-            if visit[char_id] {
-                continue;
-            }
-            visit[char_id] = true;
 
-            for second_id in ((id + 1) .. last_ids[char_id]) {
-                let second_char_id = (bytes[second_id] - 97) as usize;
+            for second_id in (first_ids[id] + 1) .. last_ids[id] {
+                let char_id = (bytes[second_id] - 97) as usize;
 
                 if count == 26 {
                     break;
                 }
-
-                if tmp_visit[second_char_id] {
+                
+                if visit & (1 << char_id) != 0 {
                     continue;
                 }
 
+                visit |= 1 << char_id;
                 count += 1;
-                tmp_visit[second_char_id] = true;
             }
-
 
             result += count;
         }
-
 
         result
     }
