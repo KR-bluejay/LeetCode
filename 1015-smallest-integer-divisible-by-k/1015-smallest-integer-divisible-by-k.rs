f67a05bf1c854f6916fd72f5c7a61638e0799010
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-25 18:18:14 +0900

    Time: 0 ms (100%), Space: 2.2 MB (31.82%) - LeetHub

---
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn smallest_repunit_div_by_k(k: i32) -> i32 {
+        let mut remainder = 0;
+
+        for id in 1 ..= k {
+            remainder = (remainder * 10 + 1) % k;
+
+            if remainder == 0 {
+                return id;
+            }
+        }
+
+        -1
+    }
+}
