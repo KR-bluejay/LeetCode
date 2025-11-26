0042fad689960aa5cde80ec0de80a49cb105f8ae
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-26 14:08:00 +0900

    Time: 68 ms (40%), Space: 12.6 MB (80%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.rs
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.rs
@@ -1,62 +1,39 @@
 impl Solution {
-    fn find_paths(
-        grid: &Vec<Vec<i32>>,
-        cache: &mut Vec<Vec<Vec<i32>>>,
-        row_id: usize,
-        col_id: usize,
-        k: i32,
-        prev_remainder: i32,
-    ) -> i32 {
-        const MODULO: i32 = 1000000007;
-
-        if grid.len() <= row_id || grid[0].len() <= col_id {
-            return 0;
-        }
-        let cur_remainder = (prev_remainder + grid[row_id][col_id]) % k;
-
-        if cache[row_id][col_id][cur_remainder as usize] != -1 {
-            return cache[row_id][col_id][cur_remainder as usize];
-        }
-
-
-        if row_id + 1 == grid.len() && col_id + 1 == grid[0].len() {
-            cache[row_id][col_id][cur_remainder as usize] = (cur_remainder == 0) as i32;
-
-            return cache[row_id][col_id][cur_remainder as usize];
-        }
-
-        let mut path_count = if row_id + 1 < grid.len() {
-            Self::find_paths(
-                grid, 
-                cache, 
-                row_id + 1,
-                col_id, 
-                k, 
-                cur_remainder
-            ) % MODULO
-        } else {
-            0
-        };
-
-        path_count += if col_id + 1 < grid[0].len() {
-            Self::find_paths(
-                grid, 
-                cache, 
-                row_id,
-                col_id + 1, 
-                k, 
-                cur_remainder,
-            ) % MODULO
-        } else {
-            0
-        };
-
-        cache[row_id][col_id][cur_remainder as usize] = path_count % MODULO;
-        cache[row_id][col_id][cur_remainder as usize]
-    }
     pub fn number_of_paths(grid: Vec<Vec<i32>>, k: i32) -> i32 {
-        let mut cache: Vec<Vec<Vec<i32>>> = vec![vec![vec![-1; k as usize]; grid[0].len()]; grid.len()];
+        const MODULO: i32 = 1000000007;
+        let k = k as usize;
 
-        Self::find_paths(&grid, &mut cache, 0, 0, k, 0)
+        let mut cache: Vec<Vec<Vec<i32>>> = vec![vec![vec![0; k]; grid[0].len()]; grid.len()];
+        let mut total_path_count = 0;
+
+        for row_id in 0 .. grid.len() {
+            for col_id in 0 .. grid[0].len() {
+                let cur_val = (grid[row_id][col_id] as usize % k);
+
+                if row_id == 0 && col_id == 0 {
+                    cache[row_id][col_id][cur_val] = 1;
+
+                    continue;
+                }
+
+                for i in 0 .. k {
+                    let prev_val = ((i + k) - cur_val) % k;
+    
+                    if row_id > 0 {
+                        cache[row_id][col_id][i] += cache[row_id - 1][col_id][prev_val];
+                    }
+    
+                    if col_id > 0 {
+                        cache[row_id][col_id][i] += cache[row_id][col_id - 1][prev_val];
+                    }
+        
+                    cache[row_id][col_id][i] %= MODULO;
+                }
+            }
+        }
+
+
+        
+        cache[grid.len() - 1][grid[0].len() - 1][0]
     }
 }
