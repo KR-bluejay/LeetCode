b9e0feb536affe66092b6bc879986be93bc8d5ad
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-26 14:28:25 +0900

    Time: 45 ms (80%), Space: 23.3 MB (60%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.rs
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.rs
@@ -1,40 +1,42 @@
 impl Solution {
     pub fn number_of_paths(grid: Vec<Vec<i32>>, k: i32) -> i32 {
-        const MODULO: i32 = 1000000007;
+        const MODULO: u32 = 1_000_000_007;
+        let (max_row_id, max_col_id) = (grid.len() - 1, grid[0].len() - 1);
         let k = k as usize;
-        let (m, n) = (grid.len(), grid[0].len());
 
-        // 이전 행과 현재 행만 유지
-        let mut prev = vec![vec![0; k]; n];
-        let mut curr = vec![vec![0; k]; n];
+        let mut prev_row: Vec<Vec<u32>> = vec![vec![0; k]; max_col_id + 1];
+        let mut cur_row: Vec<Vec<u32>> = vec![vec![0; k]; max_col_id + 1];
 
-        for row_id in 0..m {
-            for col_id in 0..n {
-                let cur_val = (grid[row_id][col_id] as usize) % k;
-
+        for row_id in 0 ..= max_row_id {
+            for col_id in 0 ..= max_col_id {
+                let cur_val = (grid[row_id][col_id] as usize % k);
+                
                 if row_id == 0 && col_id == 0 {
-                    curr[0][cur_val] = 1;
+                    cur_row[0][cur_val] = 1;
+
                     continue;
                 }
 
-                // 현재 셀 초기화
-                curr[col_id].fill(0);
-
-                for i in 0..k {
+                for i in 0 .. k {
                     let prev_val = (i + k - cur_val) % k;
-                    
+
                     if row_id > 0 {
-                        curr[col_id][i] = (curr[col_id][i] + prev[col_id][prev_val]) % MODULO;
+                        cur_row[col_id][i] += prev_row[col_id][prev_val];
                     }
-                    
                     if col_id > 0 {
-                        curr[col_id][i] = (curr[col_id][i] + curr[col_id - 1][prev_val]) % MODULO;
+                        cur_row[col_id][i] += cur_row[col_id - 1][prev_val];
                     }
+
+                    cur_row[col_id][i] %= MODULO;
                 }
             }
-            std::mem::swap(&mut prev, &mut curr);
+
+            if row_id != max_row_id {
+                std::mem::swap(&mut prev_row, &mut cur_row);
+                cur_row.iter_mut().for_each(|v| v.fill(0));
+            }
         }
 
-        prev[n - 1][0]
+        cur_row[max_col_id][0] as i32
     }
 }
