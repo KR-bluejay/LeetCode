c49474400e672b3b5356a94a4416a0d76373fa0a
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-11-27 12:09:34 +0900

    Time: 3 ms (33.33%), Space: 5.4 MB (83.33%) - LeetHub

--- 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.rs
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.rs
@@ -1,25 +1,18 @@
-
 impl Solution {
     pub fn max_subarray_sum(nums: Vec<i32>, k: i32) -> i64 {
         let sub_len = k as usize;
-        let mut prefix: Vec<i64> = Vec::with_capacity(nums.len() + 1);
-        prefix.push(0);
-
-        for id in 1 ..= nums.len() {
-            prefix.push(prefix[id - 1] + nums[id - 1] as i64);
-        }
-
-        let mut min_prefix: Vec<i64> = vec![i64::MAX; sub_len];
         let mut result = i64::MIN;
+        let mut sub_sums: Vec<i64> = vec![i64::MAX / 2; sub_len];
+        let mut prefix_sum: i64 = 0;
+        
+        sub_sums[sub_len - 1] = 0;
 
-        for id in 0 ..= nums.len() {
-            let prefix_id = id % sub_len;
+        for id in 0 .. nums.len() {
+            let remainder_id = id % sub_len;
 
-            if min_prefix[prefix_id] != i64::MAX {
-                result = result.max(prefix[id] - min_prefix[prefix_id]);
-            }
-
-            min_prefix[prefix_id] = min_prefix[prefix_id].min(prefix[id]);
+            prefix_sum += nums[id] as i64;
+            result = result.max(prefix_sum - sub_sums[remainder_id]);
+            sub_sums[remainder_id] = sub_sums[remainder_id].min(prefix_sum);
         }
 
         result
