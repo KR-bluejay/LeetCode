f408a48d99295c50c782051ecb8fb931cf65e6e2
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-12-02 18:15:24 +0900

    Time: 1 ms (23.51%), Space: 3.3 MB (72.28%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.rs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.rs
@@ -1,16 +1,17 @@
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let mut results: Vec<i32> = Vec::with_capacity(nums.len());
+        let mut results: Vec<i32> = vec![1; nums.len()];
         let mut cur = 1;
+        let mut rev_cur = 1;
 
         for id in 0 .. nums.len() {
-            results.push(cur);
-            cur *= nums[id];
-        }
-        cur = 1;
-        for id in (0 .. nums.len()).rev() {
+            let rev_id = nums.len() - 1 - id;
+            
             results[id] *= cur;
             cur *= nums[id];
+            
+            results[rev_id] *= rev_cur;
+            rev_cur *= nums[rev_id];
         }
 
         results
