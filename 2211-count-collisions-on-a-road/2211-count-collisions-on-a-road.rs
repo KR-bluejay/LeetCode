690c65e4bd1029d7d752933b4d2245c30ffc00a4
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-12-04 16:01:12 +0900

    Time: 0 ms (100%), Space: 2.4 MB (90.91%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.rs
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.rs
@@ -2,15 +2,15 @@ impl Solution {
     pub fn count_collisions(directions: String) -> i32 {
         let directions: Vec<u8> = directions.into_bytes();
         let opt_left_id: Option<usize> = directions.iter()
-            .position(|&d| d == b'S' || d == b'R');
+            .position(|&d| matches!(d, b'S' | b'R'));
         let opt_right_id: Option<usize> = directions.iter()
-            .rposition(|&d| d == b'S' || d == b'L');
+            .rposition(|&d| matches!(d, b'L' | b'S'));
 
         if let Some(left_id) = opt_left_id 
         && let Some(right_id) = opt_right_id 
         && left_id < right_id {
             directions[left_id ..= right_id].iter()
-                .fold(0, |mut acc, &d| acc + (d != b'S') as i32)
+                .fold(0, |mut acc, &d| acc + matches!(d, b'L' | b'R') as i32)
         } else {
             0
         }
