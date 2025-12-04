44a4fa8b4b8a53dca3fdc8a6ad753286a8ea70d5
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-12-04 15:58:26 +0900

    Time: 0 ms (100%), Space: 2.3 MB (90.91%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.rs
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.rs
@@ -1,44 +1,18 @@
 impl Solution {
     pub fn count_collisions(directions: String) -> i32 {
         let directions: Vec<u8> = directions.into_bytes();
-        
-        let mut collsion_count = 0;
-        // let mut collison_stack: VecDeque<u8> = VecDeque::with_capacity(direction.len());
-        let mut left_count = 0;
-        let mut stay_count = 0;
-        let mut right_count = 0;
+        let opt_left_id: Option<usize> = directions.iter()
+            .position(|&d| d == b'S' || d == b'R');
+        let opt_right_id: Option<usize> = directions.iter()
+            .rposition(|&d| d == b'S' || d == b'L');
 
-        for direction in directions.iter() {
-            match direction {
-                b'L' => {
-                    left_count = 1;
-                    if right_count > 0 {
-                        collsion_count += left_count + right_count;
-                        left_count = 0;
-                        stay_count = 1;
-                        right_count = 0;
-                    } else if stay_count > 0 {
-                        collsion_count += left_count;
-                        left_count = 0;
-                        stay_count = 1;
-                    } else {
-                        left_count = 1;
-                    }
-                },
-                b'S' => {
-                    if right_count > 0 {
-                        collsion_count += right_count;
-                    }
-                    right_count = 0;
-                    stay_count = 1;
-                },
-                b'R' => {
-                    right_count += 1;
-                },
-                _ => {}
-            }
+        if let Some(left_id) = opt_left_id 
+        && let Some(right_id) = opt_right_id 
+        && left_id < right_id {
+            directions[left_id ..= right_id].iter()
+                .fold(0, |mut acc, &d| acc + (d != b'S') as i32)
+        } else {
+            0
         }
-
-        collsion_count
     }
 }
