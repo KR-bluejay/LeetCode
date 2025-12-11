41d7424af85d24a952c594ce76e3b3a23d72af30
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-12-11 14:33:37 +0900

    Time: 27 ms (42.86%), Space: 11.3 MB (42.86%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.rs
+++ 3531-count-covered-buildings/3531-count-covered-buildings.rs
@@ -1,49 +1,27 @@
 impl Solution {
     pub fn count_covered_buildings(n: i32, mut buildings: Vec<Vec<i32>>) -> i32 {
-        buildings.sort_by(|lhs, rhs| lhs[0].cmp(&rhs[0]).then(lhs[1].cmp(&rhs[1])));
+        let city_count = n as usize + 1;
 
-        let mut building_counts: Vec<i32> = vec![0; buildings.len()];
-        let building_len = buildings.len() - 1;
-        
-        let mut x_cover: Vec<bool> = vec![false; n as usize + 1];
-        let mut x_rev_cover: Vec<bool> = vec![false; n as usize + 1];
-        
-        let mut y_cover: Vec<bool> = vec![false; n as usize + 1];
-        let mut y_rev_cover: Vec<bool> = vec![false; n as usize + 1];
+        let mut row_min = vec![usize::MAX; city_count];
+        let mut row_max = vec![usize::MIN; city_count];
 
+        let mut col_min = vec![usize::MAX; city_count];
+        let mut col_max = vec![usize::MIN; city_count];
 
-        for id in 0 .. buildings.len() {
-            let rev_id = building_len - id;
+        for building in buildings.iter() {
+            let (x_pos, y_pos) = (building[0] as usize, building[1] as usize);
 
-            let (x_pos, y_pos) = (buildings[id][0] as usize, buildings[id][1] as usize);
-            let (x_rev_pos, y_rev_pos) = (buildings[rev_id][0] as usize, buildings[rev_id][1] as usize);
+            row_min[y_pos] = row_min[y_pos].min(x_pos);
+            row_max[y_pos] = row_max[y_pos].max(x_pos);
 
-            if x_cover[x_pos] {
-                building_counts[id] += 1;
-            } else {
-                x_cover[x_pos] = true;
-            }
-
-            if y_cover[y_pos] {
-                building_counts[id] += 1;
-            } else {
-                y_cover[y_pos] = true;
-            }
-
-            if x_rev_cover[x_rev_pos] {
-                building_counts[rev_id] += 1;
-            } else {
-                x_rev_cover[x_rev_pos] = true;
-            }
-
-            if y_rev_cover[y_rev_pos] {
-                building_counts[rev_id] += 1;
-            } else {
-                y_rev_cover[y_rev_pos] = true;
-            }
-
+            col_min[x_pos] = col_min[x_pos].min(y_pos);
+            col_max[x_pos] = col_max[x_pos].max(y_pos);
         }
 
-        building_counts.into_iter().filter(|v| *v == 4).count() as i32
+        buildings.into_iter().filter(|building| {
+            let (x_pos, y_pos) = (building[0] as usize, building[1] as usize);
+
+            row_min[y_pos] < x_pos && x_pos < row_max[y_pos] && col_min[x_pos] < y_pos && y_pos < col_max[x_pos]
+        }).count() as i32
     }
 }
