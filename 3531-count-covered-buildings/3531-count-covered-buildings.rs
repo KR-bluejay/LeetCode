01aecd4496cb6cc649491ccc36826de31be7bd42
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-12-11 14:16:18 +0900

    Time: 80 ms (14.29%), Space: 11.9 MB (42.86%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.rs
+++ 3531-count-covered-buildings/3531-count-covered-buildings.rs
@@ -1,53 +1,49 @@
 impl Solution {
     pub fn count_covered_buildings(n: i32, mut buildings: Vec<Vec<i32>>) -> i32 {
-        buildings.sort_unstable_by(|lhs, rhs| lhs[0].cmp(&rhs[0]).then(lhs[1].cmp(&rhs[1])));
+        buildings.sort_by(|lhs, rhs| lhs[0].cmp(&rhs[0]).then(lhs[1].cmp(&rhs[1])));
 
-        let mut cover_counts: Vec<i32> = vec![0; buildings.len()];
-        let mut one_cover: Vec<bool> = vec![false; n as usize + 1];
-        let mut two_cover: Vec<bool> = vec![false; n as usize + 1];
+        let mut building_counts: Vec<i32> = vec![0; buildings.len()];
+        let building_len = buildings.len() - 1;
+        
+        let mut x_cover: Vec<bool> = vec![false; n as usize + 1];
+        let mut x_rev_cover: Vec<bool> = vec![false; n as usize + 1];
+        
+        let mut y_cover: Vec<bool> = vec![false; n as usize + 1];
+        let mut y_rev_cover: Vec<bool> = vec![false; n as usize + 1];
 
-        let mut result = 0;
 
-        for (id, building) in buildings.iter().enumerate() {
-            let (x_pos, y_pos) = (building[0] as usize, building[1] as usize);
+        for id in 0 .. buildings.len() {
+            let rev_id = building_len - id;
 
-            if one_cover[x_pos] {
-                cover_counts[id] += 1;
+            let (x_pos, y_pos) = (buildings[id][0] as usize, buildings[id][1] as usize);
+            let (x_rev_pos, y_rev_pos) = (buildings[rev_id][0] as usize, buildings[rev_id][1] as usize);
+
+            if x_cover[x_pos] {
+                building_counts[id] += 1;
             } else {
-                one_cover[x_pos] = true;
+                x_cover[x_pos] = true;
             }
 
-            if two_cover[y_pos] {
-                cover_counts[id] += 1;
+            if y_cover[y_pos] {
+                building_counts[id] += 1;
             } else {
-                two_cover[y_pos] = true;
+                y_cover[y_pos] = true;
             }
+
+            if x_rev_cover[x_rev_pos] {
+                building_counts[rev_id] += 1;
+            } else {
+                x_rev_cover[x_rev_pos] = true;
+            }
+
+            if y_rev_cover[y_rev_pos] {
+                building_counts[rev_id] += 1;
+            } else {
+                y_rev_cover[y_rev_pos] = true;
+            }
+
         }
 
-        one_cover.fill(false);
-        two_cover.fill(false);
-
-
-        for (id, building) in buildings.iter().enumerate().rev() {
-            let (x_pos, y_pos) = (building[0] as usize, building[1] as usize);
-
-            if one_cover[x_pos] {
-                cover_counts[id] += 1;
-            } else {
-                one_cover[x_pos] = true;
-            }
-
-            if two_cover[y_pos] {
-                cover_counts[id] += 1;
-            } else {
-                two_cover[y_pos] = true;
-            }
-
-            if cover_counts[id] == 4 {
-                result += 1;
-            }
-        }
-
-        result
+        building_counts.into_iter().filter(|v| *v == 4).count() as i32
     }
 }
