5806dcc52fcbe8b0b9247f44642a52f314826fff
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-12-12 13:19:14 +0900

    Time: 4 ms (80%), Space: 2.4 MB (20%) - LeetHub

--- 3433-count-mentions-per-user/3433-count-mentions-per-user.rs
+++ 3433-count-mentions-per-user/3433-count-mentions-per-user.rs
@@ -2,22 +2,21 @@ use std::collections::BTreeMap;
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd)]
 enum EventType {
-    ONLINE = 0,
-    OFFLINE = 1,
-    MESSAGE = 2,
+    OFFLINE = 0,
+    MESSAGE = 1,
 }
 
 impl Solution {
     pub fn count_mentions(number_of_users: i32, events: Vec<Vec<String>>) -> Vec<i32> {
         let number_of_users = number_of_users as usize;
 
-        let mut user_status: Vec<bool> = vec![true; number_of_users];
+        let mut user_status: Vec<u32> = vec![0; number_of_users];
         let mut user_mention: Vec<i32> = vec![0; number_of_users];
 
         let mut all_count = 0;
         let mut last_msg_id = 0;
 
-        let mut msg_map: BTreeMap<(usize, EventType), Vec<usize>> = BTreeMap::new();
+        let mut msg_map: BTreeMap<(u32, EventType), Vec<usize>> = BTreeMap::new();
 
         for event in events {
             let event_type: EventType = match event[0].as_str() {
@@ -26,7 +25,7 @@ impl Solution {
                 _ => unreachable!(),
             };
             
-            let event_timestamp = event[1].parse::<usize>().unwrap();
+            let event_timestamp = event[1].parse::<u32>().unwrap();
             let mentions: Vec<usize> = event[2].split_whitespace().map(|v| {
                 match v {
                     "ALL" => usize::MAX,
@@ -48,14 +47,9 @@ impl Solution {
             }
 
             msg_map.entry((event_timestamp, event_type))
-                .and_modify(|v| v.extend(&mentions.clone()))
-                .or_insert(mentions.clone());
+                .and_modify(|v| v.extend(&mentions))
+                .or_insert(mentions);
 
-            if event_type == EventType::OFFLINE {
-                msg_map.entry((event_timestamp + 60, EventType::ONLINE))
-                    .and_modify(|v| v.extend(&mentions))
-                    .or_insert(mentions);
-            }
         }
 
         for (msg_key, msg_target) in msg_map.iter() {
@@ -67,14 +61,9 @@ impl Solution {
             }
 
             match msg_type {
-                EventType::ONLINE => {
-                    for &id in msg_target.iter() {
-                        user_status[id] = true;
-                    }
-                },
                 EventType::OFFLINE => {
                     for &id in msg_target.iter() {
-                        user_status[id] = false;
+                        user_status[id] = user_status[id].max(msg_timestamp + 60);
                     }
                 },
                 EventType::MESSAGE => {
@@ -83,7 +72,7 @@ impl Solution {
                             all_count += 1;
                         } else if id == usize::MAX - 1 {
                             for (user_id, &status) in  user_status.iter().enumerate() {
-                                user_mention[user_id] += (status as i32)
+                                user_mention[user_id] += (status <= msg_timestamp ) as i32;
                             }
                         } else {
                             user_mention[id] += 1;
