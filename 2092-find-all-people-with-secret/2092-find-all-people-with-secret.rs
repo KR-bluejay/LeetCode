d2393add4b06aa2211aa9b4529dd697e47915ce9
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-12-19 11:11:28 +0900

    Time: 44 ms (75%), Space: 17.5 MB (75%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.rs
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.rs
@@ -1,101 +1,56 @@
-use std::collections::{BTreeMap, HashMap, VecDeque};
+use std::collections::{ BinaryHeap };
+use std::cmp::Reverse;
 
 impl Solution {
     pub fn find_all_people(
         n: i32, 
-        mut meetings: Vec<Vec<i32>>, 
+        meetings: Vec<Vec<i32>>, 
         first_person: i32
     ) -> Vec<i32> {
-        meetings.sort_by(|lhs, rhs| lhs[2].cmp(&rhs[2]));
-
-
-            
         let people_count = n as usize;
         let first_person = first_person as usize;
 
-        let mut secrets: Vec<bool> = vec![false; people_count];
-        
-        secrets[0] = true;
-        secrets[first_person] = true;
-
-        let mut visit: Vec<bool> = vec![false; people_count];
-        let mut share_queue: VecDeque<usize> = VecDeque::with_capacity(people_count);
-
-        let mut meeting_map: HashMap<usize, Vec<usize>> = HashMap::with_capacity(people_count);
-        let mut prev_time = 0;
+        let mut secret_heap: BinaryHeap<Reverse<(i32, usize)>> 
+            = BinaryHeap::with_capacity(people_count);
+        let mut meeting_graph: Vec<Vec<(i32, usize)>> = vec![Vec::with_capacity(5); people_count];
+        let mut has_secret: Vec<bool> = vec![false; people_count];
+        let mut secret_count = 0;
 
         for meeting in meetings {
-            let x = meeting[0] as usize;
-            let y = meeting[1] as usize;
-            let time = meeting[2] as usize;
+            let src_id = meeting[0] as usize;
+            let dst_id = meeting[1] as usize;
+            let time = meeting[2];
 
-            if prev_time != time {
-                for &id in meeting_map.keys() {
-                    if secrets[id] {
-                        share_queue.push_back(id);
-                        visit[id] = true;
-                    }
-                }
-    
-                while let Some(id) = share_queue.pop_front() {
-                    let meeting_people = meeting_map.get(&id).unwrap();
-    
-                    for &neighbor_id in meeting_people.iter() {
-                        if secrets[neighbor_id] || visit[neighbor_id] {
-                            continue;
-                        }
-    
-                        secrets[neighbor_id] = true;
-                        visit[neighbor_id] = true;
-                        share_queue.push_back(neighbor_id);
-                    }
-                }
-                meeting_map.clear();
+            meeting_graph[src_id].push((time, dst_id));
+            meeting_graph[dst_id].push((time, src_id));
+        }
+
+        secret_heap.push(Reverse((0, 0)));
+        secret_heap.push(Reverse((0, first_person)));
+
+        while let Some(Reverse((time, id))) = secret_heap.pop() {
+            if secret_count == people_count {
+                break;
             }
             
-            meeting_map.entry(x)
-                .or_insert(Vec::with_capacity(10))
-                .push(y);
-            meeting_map.entry(y)
-                .or_insert(Vec::with_capacity(10))
-                .push(x);
-
-            prev_time = time;
-        }
-
-        
-
-        for &id in meeting_map.keys() {
-            if secrets[id] {
-                share_queue.push_back(id);
-                visit[id] = true;
+            if has_secret[id] {
+                continue;
             }
-        }
+            has_secret[id] = true;
+            secret_count += 1;
 
-        while let Some(id) = share_queue.pop_front() {
-            let meeting_people = meeting_map.get(&id).unwrap();
-
-            for &neighbor_id in meeting_people.iter() {
-                if secrets[neighbor_id] || visit[neighbor_id] {
+            for &(dst_time, dst_id) in meeting_graph[id].iter() {
+                if dst_time < time || has_secret[dst_id] {
                     continue;
                 }
 
-                secrets[neighbor_id] = true;
-                visit[neighbor_id] = true;
-                share_queue.push_back(neighbor_id);
+                secret_heap.push(Reverse((dst_time, dst_id)));
             }
         }
 
-
-
-
-
-        secrets.into_iter().enumerate().filter_map(|(id, v)| {
-            if v {
-                Some(id as i32)
-            } else {
-                None
-            }
-        }).collect()
+        has_secret.into_iter()
+            .enumerate()
+            .filter_map(|(id, has_secret)| has_secret.then(|| id as i32))
+            .collect()
     }
 }
