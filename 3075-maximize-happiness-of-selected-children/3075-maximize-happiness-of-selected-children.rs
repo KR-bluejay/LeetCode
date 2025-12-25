4eba5038b9c43fabef284234de3ef24b48183780
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2025-12-25 09:38:00 +0900

    Time: 26 ms (100%), Space: 5.3 MB (100%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.rs
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.rs
@@ -2,8 +2,6 @@ impl Solution {
     pub fn maximum_happiness_sum(mut happiness: Vec<i32>, k: i32) -> i64 {
         happiness.sort_unstable_by(|left, right| right.cmp(&left));
 
-        let mut result = 0;
-
         let k = k as usize;
 
         happiness[0 .. k].into_iter().enumerate().fold(0, |acc, (id, &val)| acc + (val as i64 - id as i64).max(0))
