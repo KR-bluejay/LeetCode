53228fed21b23f76beec39241499a050398702c4
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-01-04 21:06:38 +0900

    Time: 6 ms (100%), Space: 2.3 MB (50%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.rs
+++ 1390-four-divisors/1390-four-divisors.rs
@@ -1,36 +1,33 @@
 impl Solution {
     pub fn sum_four_divisors(nums: Vec<i32>) -> i32 {
         let mut result = 0;
-        let mut divisor = Vec::with_capacity(nums.len());
 
         for num in nums.into_iter() {
             let mut num_sum = 1 + num;
+            let mut num_count = 2;
 
-            divisor.push(1);
-            divisor.push(num);
 
             for pair_num in 2 ..= num.isqrt() {
                 if num % pair_num != 0 {
                     continue;
                 }
 
+                num_count += 1;
                 num_sum += num / pair_num;
-                divisor.push(num / pair_num);
 
                 if num / pair_num != pair_num {
+                    num_count += 1;
                     num_sum += pair_num;
-                    divisor.push(pair_num);
                 }
 
-                if divisor.len() > 4 {
+                if num_count > 4 {
                     break;
                 }
             }
 
-            if divisor.len() == 4 {
+            if num_count == 4 {
                 result += num_sum;
             }
-            divisor.clear();
         }
 
         result
