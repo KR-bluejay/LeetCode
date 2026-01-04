e98d90f540d6c8abbf2b3c03bf2a398c87642fd9
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-01-04 21:03:49 +0900

    Time: 9 ms (0%), Space: 2.4 MB (0%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.rs
+++ 1390-four-divisors/1390-four-divisors.rs
@@ -1,58 +1,38 @@
-use std::collections::HashMap;
-
 impl Solution {
-    pub fn sum_four_divisors(mut nums: Vec<i32>) -> i32 {
-        nums.sort_unstable();
-
+    pub fn sum_four_divisors(nums: Vec<i32>) -> i32 {
         let mut result = 0;
-        let last_num = *nums.last().unwrap() as usize;
+        let mut divisor = Vec::with_capacity(nums.len());
 
-        let mut is_prime_nums: Vec<bool> = vec![true; last_num + 1];
-        let mut num_sieve: Vec<i32> = Vec::with_capacity(nums.len());
+        for num in nums.into_iter() {
+            let mut num_sum = 1 + num;
 
-        for i in 2 ..= last_num {
-            if !is_prime_nums[i] {
-                continue;
-            }
-            num_sieve.push(i as i32);
+            divisor.push(1);
+            divisor.push(num);
 
-            for j in 2 ..= last_num / i {
-                is_prime_nums[i * j] = false;
-            }
-        }
-
-        for num in nums {
-            let mut count = 1;
-            let mut sum = 1;
-
-            for &prime_num in num_sieve.iter() {
-                if num % prime_num != 0 || num < prime_num {
+            for pair_num in 2 ..= num.isqrt() {
+                if num % pair_num != 0 {
                     continue;
                 }
-                let mut tmp = num;
-                let mut tmp_count = 0;
-                let mut tmp_sum = 1;
 
-                while tmp % prime_num == 0 {
-                    tmp /= prime_num;
-                    tmp_count += 1;
-                    tmp_sum += prime_num.pow(tmp_count);
+                num_sum += num / pair_num;
+                divisor.push(num / pair_num);
+
+                if num / pair_num != pair_num {
+                    num_sum += pair_num;
+                    divisor.push(pair_num);
                 }
 
-                count *= (tmp_count + 1);
-                sum *= tmp_sum;
-
-                if count > 4 {
+                if divisor.len() > 4 {
                     break;
                 }
             }
 
-            if count == 4 {
-                result += sum;
+            if divisor.len() == 4 {
+                result += num_sum;
             }
+            divisor.clear();
         }
 
-        
         result
     }
 }
