7558d8920c5e24de083c262cddb5f752cd8a2ea3
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-01-08 20:20:06 +0900

    Time: 0 ms (100%), Space: 2.7 MB (66.67%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.rs
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.rs
@@ -2,7 +2,7 @@ impl Solution {
     pub fn max_dot_product(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
         let mut cache = vec![vec![i32::MIN / 2; nums2.len()]; nums1.len()];
 
-        for (num1_id, num1_val) in nums1.into_iter().enumerate() {
+        for (num1_id, &num1_val) in nums1.iter().enumerate() {
             for (num2_id, &num2_val) in nums2.iter().enumerate() {
                 let cur_prod = num1_val * num2_val;
                 let mut result = cur_prod;
