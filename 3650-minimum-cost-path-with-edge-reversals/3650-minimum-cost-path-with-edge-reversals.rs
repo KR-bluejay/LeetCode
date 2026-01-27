87e19a2badb9a549a708563b3a975e0052e5d13a
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-01-27 12:43:12 +0900

    Time: 285 ms (0%), Space: 14.6 MB (0%) - LeetHub

--- 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.rs
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.rs
@@ -28,14 +28,13 @@ impl Solution {
 
         let mut graph: Vec<Vec<(usize, i32)>> 
             = vec![Vec::with_capacity(2); n as usize];
-        let mut rev_graph: Vec<Vec<(usize, i32)>> 
-            = vec![Vec::with_capacity(2); n as usize];
+
 
         for edge in edges.into_iter() {
             let (u, v, cost) = (edge[0] as usize, edge[1] as usize, edge[2]);
 
             graph[u].push((v, cost));
-            rev_graph[v].push((u, cost * 2));
+            graph[v].push((u, cost * 2));
         }
 
         node_heap.push(Node {
@@ -44,22 +43,16 @@ impl Solution {
         });
         node_state[0] = 0;
 
-        for next_node in rev_graph[0].iter() {
-            let next_id = next_node.0;
-            let next_cost = next_node.1;
-            
-            node_heap.push(Node {
-                id: next_id,
-                cost: next_cost,
-            });
-            node_state[next_id] = next_cost;
-        }
 
         while let Some(node) = node_heap.pop() {
             let Node { id, cost } = node;
 
             if node_state[id] < cost {
                 continue;
+            }
+
+            if id == n as usize - 1 {
+                return cost;
             }
 
             for next_node in graph[id].iter() {
@@ -76,28 +69,8 @@ impl Solution {
                     cost: next_cost,
                 });
             }
-
-            for next_node in rev_graph[id].iter() {
-                let next_id = next_node.0;
-                let next_cost = next_node.1 + cost;
-
-                if node_state[next_id] <= next_cost || id == next_id {
-                    continue;
-                }
-
-                node_state[next_id] = next_cost;
-                
-                node_heap.push(Node {
-                    id: next_id,
-                    cost: next_cost,
-                });
-            }
         }
-        
-        if node_state[n as usize - 1] == i32::MAX {
-            -1
-        } else {
-            node_state[n as usize - 1] 
-        }
+
+        -1
     }
 }
