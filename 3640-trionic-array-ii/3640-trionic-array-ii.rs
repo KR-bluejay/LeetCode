a64b604e9688869192e5ac0c06a98622a8edc503
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-02-04 12:57:54 +0900

    Time: 113 ms (0%), Space: 5.1 MB (0%) - LeetHub

--- 3640-trionic-array-ii/3640-trionic-array-ii.rs
+++ 3640-trionic-array-ii/3640-trionic-array-ii.rs
@@ -1,15 +1,32 @@
 impl Solution {
     pub fn max_sum_trionic(nums: Vec<i32>) -> i64 {
         let mut center_id = 0;
+
         let mut result = i64::MIN;
 
+        let mut max_left: Vec<i64> = vec![i64::MIN; nums.len()];
+        let mut max_right: Vec<i64> = vec![i64::MIN; nums.len()];
+
+        for id in 1 .. nums.len() {
+            max_left[id] = if nums[id - 1] < nums[id] {
+                max_left[id - 1].max(0) + nums[id - 1] as i64
+            } else {
+                i64::MIN
+            };
+        }
+
+        for id in (0 .. nums.len() - 1).rev() {
+            max_right[id] = if nums[id] < nums[id + 1] {
+                max_right[id + 1].max(0) + nums[id + 1] as i64
+            } else {
+                i64::MIN
+            };
+        }
+
+
         while center_id < nums.len() {
-            let mut center_sum = nums[center_id] as i64;
-
             let (mut left_id, mut right_id) = (center_id, center_id);
-            let (mut left_sum, mut right_sum) = (0, 0);
-            let (mut max_left, mut max_right) = (i64::MIN, i64::MIN);
-
+            let mut center_sum = nums[center_id] as i64;
 
             while right_id + 1 < nums.len() && nums[right_id] > nums[right_id + 1] {
                 center_sum += nums[right_id + 1] as i64;
@@ -17,43 +34,22 @@ impl Solution {
                 right_id += 1;
             }
 
-            if center_id == right_id {
+            if left_id < right_id {
+                println!("A");
+                let left_sum = max_left[left_id];
+                let right_sum = max_right[right_id];
+
+                if left_sum != i64::MIN && right_sum != i64::MIN {
+                    println!("{left_sum} {center_sum} {right_sum}");
+                    result = result.max(left_sum + center_sum + right_sum);
+                } else {
+                    println!("{center_id} {left_sum} {right_sum}");
+                }
+
+                center_id = right_id;
+            } else {
                 center_id += 1;
-                
-                continue;
             }
-
-            let center_end = right_id;
-
-
-            while left_id > 0 && nums[left_id - 1] < nums[left_id] {
-                left_sum += nums[left_id - 1] as i64;
-                max_left = max_left.max(left_sum);
-
-                left_id -= 1;
-            }
-
-            if left_id == center_id {
-                center_id += 1;
-                
-                continue;
-            }
-
-            while right_id + 1 < nums.len() && nums[right_id] < nums[right_id + 1] {
-                right_sum += nums[right_id + 1] as i64;
-                max_right = max_right.max(right_sum);
-                
-                right_id += 1;
-            }
-
-            if center_end == right_id {
-                center_id += 1;
-
-                continue;
-            }
-
-            center_id = right_id;
-            result = result.max(max_left + center_sum + max_right);
         }
 
         result
