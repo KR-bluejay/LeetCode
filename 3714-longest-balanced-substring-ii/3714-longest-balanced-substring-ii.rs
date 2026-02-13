b008c376792acaa48e2f75be755f9092ae5dd913
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-02-13 13:28:07 +0900

    Time: 1548 ms (0%), Space: 5.9 MB (100%) - LeetHub

--- 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.rs
+++ 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.rs
@@ -2,118 +2,85 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn longest_balanced(s: String) -> i32 {
-        let bytes: Vec<u8> = s.into_bytes();
+        const CHAR_PAIRS: [(u8, u8, u8); 3] = [
+            (b'a', b'b', b'c'), 
+            (b'b', b'c', b'a'),
+            (b'c', b'a', b'b')
+        ];
         
-        let mut byte_map: HashMap<i32, i32> = HashMap::new();
-        let mut byte_sum = 1;
+        let bytes = s.into_bytes();
 
-        let mut result = 1;
+        let mut longest_len = 1;
+        let mut two_pair_map: HashMap<i32, i32> = HashMap::with_capacity(bytes.len());
 
-        for (id, &byte_item) in bytes.iter().enumerate().skip(1) {
-            byte_sum = if bytes[id - 1] == byte_item {
-                byte_sum + 1
+        two_pair_map.insert(0, -1);
+
+        let mut one_pair_len = 1;
+
+        for byte_id in 1 .. bytes.len() {
+            if bytes[byte_id - 1] == bytes[byte_id] {
+                one_pair_len += 1;
             } else {
-                1
-            };
-
-            result = result.max(byte_sum);
-        }
-        
-        byte_sum = 0;
-        byte_map.insert(0, -1);
-        
-        for (id, &byte_item) in bytes.iter().enumerate() {
-            if byte_item == b'a' {
-                byte_sum = 0;
-                byte_map.clear();
-                byte_map.insert(0, id as i32);
-
-                continue;
+                one_pair_len = 1;
             }
 
-            byte_sum += if byte_item == b'b' {
-                1
-            } else {
-                -1
-            };
-
-            if let Some(&byte_id) = byte_map.get(&byte_sum) {
-                result = result.max(id as i32 - byte_id);
-            } else {
-                byte_map.insert(byte_sum, id as i32);
-            }
-        }
-        
-        byte_sum = 0;
-        byte_map.clear();
-        byte_map.insert(0, -1);
-        
-        for (id, &byte_item) in bytes.iter().enumerate() {
-            if byte_item == b'b' {
-                byte_sum = 0;
-                byte_map.clear();
-                byte_map.insert(0, id as i32);
-
-                continue;
-            }
-
-            byte_sum += if byte_item == b'a' {
-                1
-            } else {
-                -1
-            };
-
-            if let Some(&byte_id) = byte_map.get(&byte_sum) {
-                result = result.max(id as i32 - byte_id);
-            } else {
-                byte_map.insert(byte_sum, id as i32);
-            }
-        }
-
-        byte_sum = 0;
-        byte_map.clear();
-        byte_map.insert(0, -1);
-        for (id, &byte_item) in bytes.iter().enumerate() {
-            if byte_item == b'c' {
-                byte_sum = 0;
-                byte_map.clear();
-                byte_map.insert(0, id as i32);
-
-                continue;
-            }
-
-            byte_sum += if byte_item == b'a' {
-                1
-            } else {
-                -1
-            };
-
-            if let Some(&byte_id) = byte_map.get(&byte_sum) {
-                result = result.max(id as i32 - byte_id);
-            } else {
-                byte_map.insert(byte_sum, id as i32);
-            }
-        }
-
-        let mut third_map: HashMap<(i32, i32), i32> = HashMap::new();
-        third_map.insert((0, 0), -1);
-
-        let mut freq: [i32; 3] = [0, 0, 0];
-
-        for (byte_id, &byte_item) in bytes.iter().enumerate() {
-            freq[(byte_item - b'a') as usize] += 1;
-            
-            let map_key = (freq[0] - freq[1], freq[1] - freq[2]);
-
-            if let Some(id) = third_map.get(&map_key) {
-                result = result.max(byte_id as i32 - id);
-            } else {
-                third_map.insert(map_key, byte_id as i32);
-            }
+            longest_len = longest_len.max(one_pair_len);
         }
 
 
 
-        result
+
+        for &(pair_1, pair_2, wall) in CHAR_PAIRS.iter() {
+            let mut pair_sum = 0;
+
+            for (id, &b) in bytes.iter().enumerate() {
+                if b == wall {
+                    pair_sum = 0;
+
+                    two_pair_map.clear();
+                    two_pair_map.insert(0, id as i32);
+
+                    continue;
+                }
+
+                if b == pair_1 {
+                    pair_sum += 1; 
+                } else {
+                    pair_sum -= 1;
+                }
+
+                if let Some(pair_id) = two_pair_map.get(&pair_sum) {
+                    longest_len = longest_len.max(id as i32 - pair_id);
+                } else {
+                    two_pair_map.insert(pair_sum, id as i32);
+                }
+            }
+
+            two_pair_map.clear();
+            two_pair_map.insert(0, -1);
+        }
+
+        let mut pair_counts: [i32; 3] = [0, 0, 0];
+        let mut third_pair_map: HashMap<(i32, i32, i32), i32> = HashMap::with_capacity(bytes.len());
+
+        third_pair_map.insert((0, 0, 0), -1);
+
+        for (id, &b) in bytes.iter().enumerate() {
+            pair_counts[(b - b'a') as usize] += 1;
+
+            let pair_key = (
+                pair_counts[0] - pair_counts[1], 
+                pair_counts[1] - pair_counts[2], 
+                pair_counts[2] - pair_counts[0],
+            );
+
+            if let Some(pair_id) = third_pair_map.get(&pair_key) {
+                longest_len = longest_len.max(id as i32 - pair_id);
+            } else {
+                third_pair_map.insert(pair_key, id as i32);
+            }
+        }
+
+        longest_len
     }
 }
