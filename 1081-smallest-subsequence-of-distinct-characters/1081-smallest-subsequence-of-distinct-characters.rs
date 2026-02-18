b385ecd3ba9105d1c40d2a869726c528e2b16319
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-02-18 17:21:39 +0900

    Time: 0 ms (100%), Space: 2.1 MB (44.44%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.rs
@@ -0,0 +1,33 @@
+impl Solution {
+    pub fn smallest_subsequence(s: String) -> String {
+        let s_bytes = s.into_bytes();
+
+        let mut byte_seen: u32 = 0;
+        let mut byte_pos = vec![0; 26];
+        let mut byte_stack: Vec<u8> = Vec::with_capacity(s_bytes.len());
+        
+        for (id, &s_byte) in s_bytes.iter().enumerate() {
+            byte_pos[(s_byte - b'a') as usize] = id;
+        }
+
+        for (id, s_byte) in s_bytes.into_iter().enumerate() {
+            let s_id = s_byte - b'a';
+
+            if byte_seen & (1 << s_id) != 0 {
+                continue;
+            }
+
+            while let Some(&last_char) = byte_stack.last() 
+            && last_char > s_byte 
+            && byte_pos[(last_char - b'a') as usize] > id {
+                byte_stack.pop();
+                byte_seen &= !(1 << (last_char - b'a'));
+            }
+            byte_stack.push(s_byte);
+            byte_seen |= (1 << s_id);
+        }
+
+        unsafe{String::from_utf8_unchecked(byte_stack)}
+        
+    }
+}
