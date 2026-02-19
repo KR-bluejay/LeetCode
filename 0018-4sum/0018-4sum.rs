96e07d284321c64bb644bb68a600b94f4b0ea757
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-02-19 15:39:00 +0900

    Time: 1 ms (83.46%), Space: 2.3 MB (45.86%) - LeetHub

--- 0018-4sum/0018-4sum.rs
+++ 0018-4sum/0018-4sum.rs
@@ -7,32 +7,52 @@ impl Solution {
         if nums.len() < 4 {
             return vec![];
         }
-        
+
         nums.sort_unstable();
 
-        let mut num_id_set: HashSet<(i32, i32, i32, i32)> 
-            = HashSet::with_capacity(nums.len());
+        let target = target as i64;
+
+        let mut num_id_set: Vec<Vec<i32>> 
+            = Vec::with_capacity(nums.len());
 
         for i in 0 .. nums.len() - 3 {
+            if 0 < i && nums[i] == nums[i - 1] {
+                continue;
+            }
+
             for j in (i + 1) .. nums.len() - 2 {
-                let preset = nums[i] + nums[j];
+                if i + 1 < j && nums[j] == nums[j - 1] {
+                    continue;
+                }
+
+                let preset = nums[i] as i64 + nums[j] as i64;
 
                 let mut left_id = j + 1;
                 let mut right_id = nums.len() - 1;
 
                 while left_id < right_id {
-                    let sum = preset + nums[left_id] + nums[right_id];
+                    let sum = preset + nums[left_id] as i64 + nums[right_id] as i64;
 
                     match sum.cmp(&target) {
                         Ordering::Less => left_id += 1,
                         Ordering::Greater => right_id -= 1,
                         Ordering::Equal => {
-                            num_id_set.insert((
+                            num_id_set.push(vec![
                                 nums[i], 
                                 nums[j], 
                                 nums[left_id], 
                                 nums[right_id],
-                            ));
+                            ]);
+
+                            while left_id < right_id
+                            && nums[left_id] == nums[left_id + 1] {
+                                left_id += 1;
+                            }
+
+                            while left_id < right_id
+                            && nums[right_id - 1] == nums[right_id] {
+                                right_id -= 1;
+                            }
 
                             left_id += 1;
                             right_id -= 1;
@@ -42,8 +62,6 @@ impl Solution {
             }
         }
 
-        num_id_set.into_iter()
-            .map(|(a1, a2, a3, a4)| vec![a1, a2, a3, a4])
-            .collect()
+        num_id_set
     }
 }
