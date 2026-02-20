9c8b597b22a19abc1fb7a4e16e7033c54114eaf8
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-02-20 22:42:05 +0900

    Time: 7 ms (87.5%), Space: 5.4 MB (0%) - LeetHub

---
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn count_nice_pairs(mut nums: Vec<i32>) -> i32 {
+        const MODULO: i64 = 1_000_000_007;
+
+        
+        let mut result: i64 = 0;
+        let mut num_map: HashMap<i32, i64> = HashMap::with_capacity(nums.len());;
+
+        for num in nums.into_iter() {
+            let mut origin_num = num;
+            let mut rev_num = 0;
+
+            while origin_num > 0 {
+                rev_num = rev_num * 10 + (origin_num % 10);
+                origin_num /= 10;
+            }
+
+
+            num_map.entry(num - rev_num)
+                .and_modify(|v| *v += 1)
+                .or_insert(1);
+        }
+
+
+
+        for count in num_map.into_values() {
+            result += count * (count - 1) / 2;
+            result %= MODULO;
+        }
+
+
+
+        result as i32
+    }
+}
