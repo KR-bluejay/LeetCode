f3058eb74246874d61c4d9339dc8cb958b53a31c
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-02-21 14:09:02 +0900

    Time: 0 ms (100%), Space: 2.2 MB (8.57%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.rs
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.rs
@@ -1,24 +1,21 @@
 impl Solution {
     pub fn count_prime_set_bits(left: i32, right: i32) -> i32 {
-        let (left, right) = (left as usize, right as usize);
-        let mut prime_nums: [bool; 32] = [true; 32];
+        const PRIME_BIT: u32 = 1 << 2 
+            | 1 << 3 
+            | 1 << 5 
+            | 1 << 7 
+            | 1 << 11 
+            | 1 << 13 
+            | 1 << 17 
+            | 1 << 19 
+            | 1 << 23
+            | 1 << 29 
+            | 1 << 31;
+
         let mut result = 0;
 
-        prime_nums[0] = false;
-        prime_nums[1] = false;
-
-        for id in 2 .. 32 {
-            if !prime_nums[id] {
-                continue;
-            }
-
-            for mul in 2 .. (32 / id) {
-                prime_nums[id * mul] = false;
-            }
-        }
-
-        for num in left ..= right {
-            result += prime_nums[num.count_ones() as usize] as i32;
+        for mid in left ..= right {
+            result += ((PRIME_BIT >> mid.count_ones()) & 1) as i32;
         }
 
         result
