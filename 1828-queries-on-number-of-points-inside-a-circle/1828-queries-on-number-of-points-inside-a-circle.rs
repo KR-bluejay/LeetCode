aa9c47c54b729e056587c9bf4b2a8828fc9bc0b3
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-03-08 21:58:02 +0900

    Time: 6 ms (26.92%), Space: 2.2 MB (100%) - LeetHub

---
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    pub fn count_points(points: Vec<Vec<i32>>, queries: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut results: Vec<i32> = Vec::with_capacity(queries.len());
+        
+        for query in queries.iter() {
+            let mut result = 0;
+            let (qx, qy, qr) = (query[0], query[1], query[2]);
+            let qr2 = qr.pow(2);
+        
+            for point in points.iter() {
+                let (px, py) = (point[0], point[1]);
+                let (dx, dy) = (qx - px, qy - py);
+                
+                result += (qr2 >= (dx.pow(2) + dy.pow(2))) as i32;
+            }
+
+            results.push(result);
+        }
+
+        results
+    }
+}
