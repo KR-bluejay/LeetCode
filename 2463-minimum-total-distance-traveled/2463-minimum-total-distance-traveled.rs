1d27cf4e1cd831c624cfa0777972aeca36be91e4
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-04-14 14:58:58 +0900

    Time: 16 ms (100%), Space: 10 MB (0%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.rs
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.rs
@@ -1,68 +1,36 @@
+use std::iter::repeat_n;
+
 impl Solution {
-    pub fn minimum_total_distance(
-        mut robots: Vec<i32>, 
-        mut factories: Vec<Vec<i32>>
-    ) -> i64 {
+    pub fn minimum_total_distance(mut robots: Vec<i32>, mut factories: Vec<Vec<i32>>) -> i64 {
         robots.sort_unstable();
         factories.sort_unstable();
 
-        let mut factory_pos = Vec::with_capacity(robots.len());
-
+        let mut factory_positions = Vec::with_capacity(robots.len());
 
         for factory in factories.into_iter() {
-            let (pos, limit) = (factory[0], factory[1]);
+            let (position, limit) = (factory[0], factory[1] as usize);
 
-            for _ in 0 .. limit {
-                factory_pos.push(pos);
+            factory_positions.extend(repeat_n(position, limit));
+        }
+
+        let mut min_dist_cache = vec![vec![i64::MAX >> 2; factory_positions.len() + 1]; robots.len() + 1];
+
+        for factory_id in 0 ..= factory_positions.len() {
+            min_dist_cache[0][factory_id] = 0;
+        }
+
+        for robot_id in 1 ..= robots.len() {
+            for factory_id in robot_id ..= factory_positions.len() {
+                let dist = (robots[robot_id - 1] - factory_positions[factory_id - 1]).abs() as i64;
+
+                let assign_cost = min_dist_cache[robot_id - 1][factory_id - 1] + dist;
+                let skip_cost = min_dist_cache[robot_id][factory_id - 1];
+
+                min_dist_cache[robot_id][factory_id] = assign_cost.min(skip_cost);
             }
         }
 
-        let mut cache = vec![vec![-1; factory_pos.len()]; robots.len()];
 
-        Self::calc(&mut cache, &robots, 0, &factory_pos, 0);
-
-        cache[0][0]
+        *min_dist_cache.last().unwrap().last().unwrap()
     }
-    fn calc(
-        cache: &mut Vec<Vec<i64>>,
-        robots: &Vec<i32>,
-        robot_id: usize,
-        factories: &Vec<i32>,
-        factory_id: usize,
-    ) -> i64 {
-        if robot_id == robots.len() {
-            return 0;
-        }
-
-        if factory_id == factories.len() {
-            return i64::MAX / 2;
-        }
-
-        if cache[robot_id][factory_id] != -1 {
-            return cache[robot_id][factory_id];
-        }
-
-        let cur_dist = (robots[robot_id] - factories[factory_id])
-            .abs() as i64;
-        
-        let assign = cur_dist + Self::calc(
-            cache,
-            robots, 
-            robot_id + 1, 
-            factories, 
-            factory_id + 1
-        );
-        let skip = Self::calc(
-            cache,
-            robots,
-            robot_id,
-            factories,
-            factory_id + 1,
-        );
-
-
-        cache[robot_id][factory_id] = assign.min(skip);
-        cache[robot_id][factory_id]
-    }
-
 }
