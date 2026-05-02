cd84a48ef785b6c82b5de3841c0afb4a3db593a5
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-05-02 12:03:04 +0900

    Time: 0 ms (100%), Space: 2 MB (85.71%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.rs
@@ -0,0 +1,29 @@
+impl Solution {
+    pub fn rotated_digits(n: i32) -> i32 {
+        let mut result = 0;
+        let invalid_rotate = [3, 4, 7];
+
+        'outer: for i in 1 ..= n {
+            let mut must_digit = false;
+            let mut num = i;
+
+            while num > 0 {
+                let t = num % 10;
+
+                if t == 3 || t == 4 || t == 7 {
+                    continue 'outer;
+                }
+
+                must_digit |= (t == 2 || t == 5 || t == 6 || t == 9);
+                num /= 10;
+            }
+
+            if must_digit {
+                result += 1;
+            }
+        }
+
+
+        result
+    }
+}
