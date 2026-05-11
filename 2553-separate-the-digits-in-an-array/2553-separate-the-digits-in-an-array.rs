707f4af1a51cd4ce6967ce7796a5aceca40948ea
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-05-11 14:01:36 +0900

    Time: 0 ms (100%), Space: 2.2 MB (94.74%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.rs
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.rs
@@ -1,10 +1,20 @@
 impl Solution {
     pub fn separate_digits(nums: Vec<i32>) -> Vec<i32> {
-        let mut result = Vec::with_capacity(nums.len());
+        let mut result = Vec::with_capacity(nums.len() * 6);
 
-        for num in nums.into_iter() {
-            for num_byte in num.to_string().into_bytes() {
-                result.push((num_byte - b'0') as i32);
+        for mut num in nums.into_iter() {
+            let num_len = num.ilog10() as usize + 1;
+
+            let start_id = result.len();
+            let end_id = result.len() + num_len;
+
+            unsafe {
+                result.set_len(result.len() + num_len);
+            }
+
+            for id in (start_id .. end_id).rev() {
+                result[id] = num % 10;
+                num /= 10;
             }
         }
 
