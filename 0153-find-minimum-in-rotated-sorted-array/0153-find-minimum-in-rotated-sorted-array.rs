93147dee786a1c6f491d401c953686a11729b684
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-05-15 11:05:02 +0900

    Time: 0 ms (100%), Space: 2.1 MB (71.08%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.rs
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.rs
@@ -1,19 +1,6 @@
 impl Solution {
-    pub fn find_min(nums: Vec<i32>) -> i32 {
-        let mut left = 0;
-        let mut right = nums.len() - 1;
-
-
-        while left < right {
-            let mid = left + (right - left) / 2;
-
-            if nums[mid] > nums[right] {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-
-        nums[left]
+    pub fn find_min(mut nums: Vec<i32>) -> i32 {
+        nums.sort_unstable();
+        nums[0]
     }
 }
