c964276a32167310dffa16e1b5823b687de12246
KR-bluejay <kr.bluejay.01.dev@gmail.com>
2026-05-27 11:18:09 +0900

    Time: 19 ms (0%), Space: 2.5 MB (100%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.rs
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.rs
@@ -1,25 +1,13 @@
 impl Solution {
     pub fn number_of_special_chars(word: String) -> i32 {
-        let mut cache = vec![false; 52];
+        let word = word.into_bytes();
         let mut result = 0;
 
-        for word_byte in word.into_bytes() {
-            if word_byte <= b'Z' {
-                let id = (word_byte - b'A' + 26) as usize;
-                let rev_id = (word_byte - b'A') as usize;
-
-                if cache[rev_id] && !cache[id] {
-                    result += 1;
-                }
-                cache[id] = true;
-            } else {
-                let id = (word_byte - b'a') as usize;
-                let rev_id = (word_byte - b'a' + 26) as usize;
-
-                if cache[id] && cache[rev_id] {
-                    result -= 1;
-                }
-                cache[id] = !cache[rev_id];
+        for byte_id in b'a' ..= b'z' {
+            if let Some(lower_id) = word.iter().rposition(|&w| w == byte_id) 
+            && let Some(upper_id) = word.iter().position(|&w| w == byte_id - 32) 
+            && lower_id < upper_id {
+                result += 1;
             }
         }
 
